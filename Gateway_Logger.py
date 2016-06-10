b67d6a9aba7002783138fa2730c1003c08adcc01
worriedmedic <comical115@gmail.com>
2016-06-10 12:10:25 -0400

    Update Gateway_Logger.py

--- Gateway_Logger.py
+++ Gateway_Logger.py
@@ -10,8 +10,9 @@ import requests
 import os.path
 
 addr                = '/dev/tty.usbserial-A104VAFG'  # serial port to read data from
-baud                = 9600            # baud rate for serial port
-thingspeak_update   = 'true' # 'true' or 'false' to turn on/off updating to ThingSpeak
+baud                = 9600   # baud rate for serial port
+thingspeak_update   = 'true' # Turn on/off updating to ThingSpeak
+emoncms_update      = 'true' # Turn on/off updating to Emoncms
 txt_logging         = 'true' # Enable/Disable logging to TXT file
 verbose             = 'true'
 
@@ -34,6 +35,7 @@ with serial.Serial(addr,9600) as pt:
 
         addr  = buffer[0:2]
 
+        #Prefix 0 == Cuttyhunk Atmospheric Sensors
         if addr.startswith('0'):
             buffer.split(',)
             temp = buffer.split(',')[1].strip('T')
@@ -41,20 +43,23 @@ with serial.Serial(addr,9600) as pt:
             humid = buffer.split(',')[3].strip('H')
             volt = buffer.split(',')[4].strip('V')
             rssi = buffer.split(',')[5].strip('\n')
-            #temp  = buffer[4:9]
-            #press = buffer[11:16]
-            #humid = buffer[18:23]
-            #volt  = buffer[25:30]
-            #rssi  = buffer[31:34]
+            
+        #Prefix 1 == Dover Atmospheric Sensors
+        elif addr.startswith('1'): 
+            buffer.split(',)
+            temp = buffer.split(',')[1].strip('T')
+            press = buffer.split(',')[2].strip('P')
+            humid = buffer.split(',')[3].strip('H')
+            volt = buffer.split(',')[4].strip('V')
+            rssi = buffer.split(',')[5].strip('\n')
 
+        #Prefix A == Dover Power Sensors
         elif addr.startswith('A'):
             buffer.split(',')
             total_watts = buffer.split(',')[1].strip('TW')
             watts = buffer.split(',')[2].strip('WC')
             volt = buffer.split(',')[4].strip('V')
             rssi = buffer.split(',')[5].strip('\n')
-
-
 
         if txt_logging == 'true':
             fname = str(today) + '.log'  # log file to save data in
@@ -70,37 +75,39 @@ with serial.Serial(addr,9600) as pt:
 
         if thingspeak_update == 'true':
             url = 'https://api.thingspeak.com/update.json'
+            temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field5': volt, 'field6': rssi}
+            power_payload = {'api_key': api_key, 'field1': addr, 'field2': watts, 'field4': volt, 'field5': rssi}
 
-            if addr == '00':
+            if addr == '00': #
                 api_key = 'TFGVV0YYM18ALONJ'
-                payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid,
-                       'field5': volt, 'field6': rssi}
-                r = requests.post(url,data=payload)
+                r = requests.post(url,data=temp_payload)
                 if verbose == 'true':
                     print(r.text)
 
             elif addr == '01':
                 api_key = 'ARPQ7GWOHTQSYWYW'
-                payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid,
-                       'field5': volt, 'field6': rssi}
-                r = requests.post(url, data=payload)
+                r = requests.post(url, data=temp_payload)
                 if verbose == 'true':
                     print(r.text)
                     
             elif addr == '09':
                 api_key = 'TUFQWU8SA1HL1B4O'
-                payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid,
-                       'field5': volt, 'field6': rssi}
-                r = requests.post(url, data=payload)
+                r = requests.post(url, data=temp_payload)
                 if verbose == 'true':
                     print(r.text)
 
             elif addr == 'A1':
                 api_key = '2I106Q4EPCT9228E'
-                payload = {'api_key': api_key, 'field1': total_watts, 'field2': watts, 'field3': avg_watts, 'field4': volt, 'field5': rssi}
-                r = requests.post(url, data=payload)
+                r = requests.post(url, data=power_payload)
                 if verbose == 'true':
                     print(r.text)
 
             else:
                 print("SENSOR ID NOT FOUND")
+        
+        if emoncms_update == 'true':
+            url = 	'https://emoncms.org/feed/update.json?'
+            api_key = '4e6eff5d047580696f0e2a7ae9323983'
+            payload = {'Temperature': temp, 'Pressure': press, 'Humidity': humid, 'Voltage': volt, 'RSSI', rssi}
+            
+            
