a09b0aa19e604ffdb43685940f0a08711f25df66
worriedmedic <comical115@gmail.com>
2016-06-24 15:24:32 -0400

    Update Power_Monitor.py

--- Power_Monitor.py
+++ Power_Monitor.py
@@ -65,6 +65,7 @@ with serial.Serial(addr,9600) as pt:
             if addr == '10':
                 api_key = '2I106Q4EPCT9228E'
                 power_payload = {'api_key': api_key, 'field1': ct1p, 'field2': ct2p, 'field3': ct3p, 'field4': ct4p, 'field5': volt}
+                time.sleep(1)
                 r = requests.post(url, data=power_payload)
                 if r.text == "0":
                     print("Thingspeak Update FAILED")
