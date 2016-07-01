03f12cc7369b0c4424ffad6c98188ce5a3912787
worriedmedic <comical115@gmail.com>
2016-07-01 14:48:28 -0400

    Update Power_Monitor.py

--- Power_Monitor.py
+++ Power_Monitor.py
@@ -35,60 +35,63 @@ with serial.Serial(addr,9600) as pt:
 
         addr = '10'
         
-        buffer.split(' ')
-        ct1p = buffer.split(' ')[0]
-        ct2p = buffer.split(' ')[1]
-        ct3p = buffer.split(' ')[2]
-        ct4p = buffer.split(' ')[3]
-        volt = buffer.split(' ')[4]
+        try:
+            buffer.split(' ')
+            ct1p = buffer.split(' ')[0]
+            ct2p = buffer.split(' ')[1]
+            ct3p = buffer.split(' ')[2]
+            ct4p = buffer.split(' ')[3]
+            volt = buffer.split(' ')[4]
         
-        volt = float(volt) / 100
+            volt = float(volt) / 100
             
-        ### Check output of above split ###
-        if verbose == 'true':
-            print(ct1p,ct2p,ct3p,ct4p,volt) 
+            ### Check output of above split ###
+            if verbose == 'true':
+                print(ct1p,ct2p,ct3p,ct4p,volt) 
             
-        if txt_logging == 'true':
-            fname = str(today) + 'POWER.log'  # log file to save data in
-            fdirectory = 'data_log'
-            fmode = 'a'  # log file mode = append
+            if txt_logging == 'true':
+                fname = str(today) + 'POWER.log'  # log file to save data in
+                fdirectory = 'data_log'
+                fmode = 'a'  # log file mode = append
 
-            if not os.path.exists(fdirectory):
-                os.makedirs(fdirectory)
+                if not os.path.exists(fdirectory):
+                    os.makedirs(fdirectory)
 
-            outf = open(os.path.join(fdirectory, fname), fmode)
-            outf.write(x)  # write line of text to file
-            outf.flush()  # make sure it actually gets written out
+                outf = open(os.path.join(fdirectory, fname), fmode)
+                outf.write(x)  # write line of text to file
+                outf.flush()  # make sure it actually gets written out
 
-        if emoncms_update == 'true':
-            try:
-                url = 'https://emoncms.org/input/post.json?node=%s&json={CT1:%s,CT2:%s,CT3:%s,CT4:%s,VOLT:%s}&apikey=4e6eff5d047580696f0e2a7ae9323983' % (addr, ct1p, ct2p, ct3p, ct4p, volt)
-                r = requests.post(url)
-                if "ok" in r:
-                    print("EMONCMS Update OK")
-                else:
-                    print("EMCONMS Update FAILED")
-                if verbose == 'true':
-                    print(r.text)
-            except requests.exceptions.RequestException as e:
-                print(e.text)
-                
-        if thingspeak_update == 'true':
-            url = 'https://api.thingspeak.com/update.json'
-
-            if addr == '10':
+            if emoncms_update == 'true':
                 try:
-                    api_key = '2I106Q4EPCT9228E'
-                    power_payload = {'api_key': api_key, 'field1': ct1p, 'field2': ct2p, 'field3': ct3p, 'field4': ct4p, 'field5': volt}
-                    r = requests.post(url, data=power_payload)
-                    if r.text == "0":
-                        print("Thingspeak Update FAILED")
+                    url = 'https://emoncms.org/input/post.json?node=%s&json={CT1:%s,CT2:%s,CT3:%s,CT4:%s,VOLT:%s}&apikey=4e6eff5d047580696f0e2a7ae9323983' % (addr, ct1p, ct2p, ct3p, ct4p, volt)
+                    r = requests.post(url)
+                    if "ok" in r:
+                        print("EMONCMS Update OK")
                     else:
-                        print("Thingspeak Update OK")
+                        print("EMCONMS Update FAILED")
                     if verbose == 'true':
                         print(r.text)
                 except requests.exceptions.RequestException as e:
                     print(e.text)
+                
+            if thingspeak_update == 'true':
+                url = 'https://api.thingspeak.com/update.json'
+
+                if addr == '10':
+                    try:
+                        api_key = '2I106Q4EPCT9228E'
+                        power_payload = {'api_key': api_key, 'field1': ct1p, 'field2': ct2p, 'field3': ct3p, 'field4': ct4p, 'field5': volt}
+                        r = requests.post(url, data=power_payload)
+                        if r.text == "0":
+                            print("Thingspeak Update FAILED")
+                        else:
+                            print("Thingspeak Update OK")
+                        if verbose == 'true':
+                            print(r.text)
+                    except requests.exceptions.RequestException as e:
+                        print(e.text)
             
-            else:
-                print("SENSOR ID NOT FOUND")
+                else:
+                    print("SENSOR ID NOT FOUND")
+        except:
+            print("Generic Error encountered")
