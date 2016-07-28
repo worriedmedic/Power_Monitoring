5d769c5e9bf5c208fe75a5938f321c18fd58657f
worriedmedic <comical115@gmail.com>
2016-07-28 08:22:39 -0400

    Update Power_Monitor.py

--- Power_Monitor.py
+++ Power_Monitor.py
@@ -82,12 +82,16 @@ with serial.Serial(addr,9600) as pt:
             try:
                 url = 'https://emoncms.org/input/post.json?node=%s&json={CTT:%s,CT1:%s,CT2:%s,CT3:%s,CT4:%s,VOLT:%s}&apikey=4e6eff5d047580696f0e2a7ae9323983' % (addr, cttotal, ct1p, ct2p, ct3p, ct4p, volt)
                 r = requests.post(url)
+                if "ok" in r:
+                    print("EMONCMS Update OK", r)
+                else:
+                    print("EMCONMS Update FAILED", r)
                 if verbose == 'true':
                     print(r.text)
                     if "ok" in r:
-                        print("EMONCMS Update OK")
+                        print("EMONCMS Update OK", r)
                     else:
-                        print("EMCONMS Update FAILED")
+                        print("EMCONMS Update FAILED", r)
 
             except requests.exceptions.RequestException as e:
                 print("EMONCMS REQUESTS ERROR", today, now, e, "::", buffer)
