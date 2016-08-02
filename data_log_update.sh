dc49379c4b33adf9cba21e0bdefc62164c84ac3d
worriedmedic <comical115@gmail.com>
2016-08-01 20:59:41 -0400

    Add files via upload

---
+++ data_log_update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#Script to update log files to dropbox
+
+/home/pi/Downloads/Dropbox-Uploader/dropbox_uploader.sh -p upload ~/Power_Monitoring/data_log/ /Programing/
+/home/pi/Downloads/Dropbox-Uploader/dropbox_uploader.sh -p upload /var/log/network_restart.log /Programing/data_log/
+
+find /home/pi/Power_Monitoring/data_log/* -mtime +7 -delete
