b5ff50b7c80b5a268cabd13dd5eaff4ef47d6456
worriedmedic <comical115@gmail.com>
2016-08-01 21:53:28 -0400

    Update Gateway_Logger.py

--- Gateway_Logger.py
+++ Gateway_Logger.py
@@ -55,6 +55,9 @@ with serial.Serial(addr,9600) as pt:
                 humid = buffer.split(',')[3].strip('H')
                 volt = buffer.split(',')[4].strip('V')
                 rssi = buffer.split(',')[5]
+                
+                dew = float(temp) - ((100 - float(humid)) / 5 ) ##FROM DATA PROCESSING PYTHON SCRIPT
+                
             except Exception as e:
                 print("DATA SPLIT ERROR", today, now, e, "::", buffer)
             
@@ -75,7 +78,7 @@ with serial.Serial(addr,9600) as pt:
 
         if emoncms_update == 'true':
             try:
-                url = 'https://emoncms.org/input/post.json?node=%s&json={T:%s,P:%s,H:%s,V:%s,R:%s}&apikey=4e6eff5d047580696f0e2a7ae9323983' % (addr, temp, press, humid, volt, rssi)
+                url = 'https://emoncms.org/input/post.json?node=%s&json={T:%s,P:%s,H:%s,V:%s,R:%s,D:%s}&apikey=4e6eff5d047580696f0e2a7ae9323983' % (addr, temp, press, humid, volt, rssi, dew)
                 r = requests.post(url)
                 if verbose == 'true':
                     print(r.text)
@@ -94,7 +97,7 @@ with serial.Serial(addr,9600) as pt:
             if addr == '00':
                 try:
                     api_key = 'TFGVV0YYM18ALONJ'
-                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field6': volt, 'field7': rssi}
+                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field5': dew, 'field6': volt, 'field7': rssi}
                     r = requests.post(url,data=temp_payload)
                     if verbose == 'true':
                         print(r.text)
@@ -111,7 +114,7 @@ with serial.Serial(addr,9600) as pt:
             elif addr == '01':
                 try:
                     api_key = 'ARPQ7GWOHTQSYWYW'
-                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field6': volt, 'field7': rssi}
+                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field5': dew, 'field6': volt, 'field7': rssi}
                     r = requests.post(url, data=temp_payload)
                     if verbose == 'true':
                         print(r.text)
@@ -128,7 +131,7 @@ with serial.Serial(addr,9600) as pt:
             elif addr == '05':
                 try:
                     api_key = '89NM6222ST0UW15H'
-                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field6': volt, 'field7': rssi}
+                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field5': dew, 'field6': volt, 'field7': rssi}
                     r = requests.post(url, data=temp_payload)
                     if verbose == 'true':
                         print(r.text)
@@ -145,7 +148,7 @@ with serial.Serial(addr,9600) as pt:
             elif addr == '06':
                 try:
                     api_key = 'LZAFORDCZ4UT75GU'
-                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field6': volt, 'field7': rssi}
+                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field5': dew, 'field6': volt, 'field7': rssi}
                     r = requests.post(url, data=temp_payload)
                     if verbose == 'true':
                         print(r.text)
@@ -162,7 +165,7 @@ with serial.Serial(addr,9600) as pt:
             elif addr == '07':
                 try:
                     api_key = 'NQQZE8CL8ZC445DN'
-                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field6': volt, 'field7': rssi}
+                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field5': dew, 'field6': volt, 'field7': rssi}
                     r = requests.post(url, data=temp_payload)
                     if verbose == 'true':
                         print(r.text)
@@ -179,7 +182,7 @@ with serial.Serial(addr,9600) as pt:
             elif addr == '08':
                 try:
                     api_key = '8SHTGBFETA4XVN5P'
-                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field6': volt, 'field7': rssi}
+                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field5': dew, 'field6': volt, 'field7': rssi}
                     r = requests.post(url, data=temp_payload)
                     if verbose == 'true':
                         print(r.text)
@@ -196,7 +199,7 @@ with serial.Serial(addr,9600) as pt:
             elif addr == '09':
                 try:
                     api_key = 'TUFQWU8SA1HL1B4O'
-                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field6': volt, 'field7': rssi}
+                    temp_payload = {'api_key': api_key, 'field1': addr, 'field2': temp, 'field3': press, 'field4': humid, 'field5': dew, 'field6': volt, 'field7': rssi}
                     r = requests.post(url, data=temp_payload)
                     if verbose == 'true':
                         print(r.text)
