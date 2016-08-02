725c13b6df1d04a8f8e0bed7fb2b3add378ebdf9
worriedmedic <comical115@gmail.com>
2016-08-02 13:14:29 -0400

    Update SVG_PNG_Script.sh

--- SVG_PNG_Script.sh
+++ SVG_PNG_Script.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-cd "$(dirname "$0")"
+cd /home/pi/Power_Monitoring
 
 while :
 do
-	rsvg-convert -b white -o /home/pi/Programing/Cuttyhunk_Wx_St/SVG_Kindle/weather-script-output.png /home/pi/Programing/Cuttyhunk_Wx_St/Data_Process/TEST.svg
+	rsvg-convert -b white -o weather-script-output.png TEST.svg
 	pngcrush -c 0 -ow weather-script-output.png
 	sudo cp -f weather-script-output.png /var/www/html/weather-script-output.png
 	sudo chmod 755 /var/www/html/weather-script-output.png
