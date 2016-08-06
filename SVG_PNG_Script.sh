46e7a7c2f82d080d3c30a38f4dd38ec505a27394
worriedmedic <comical115@gmail.com>
2016-08-06 18:20:23 -0400

    modified:   SVG_PNG_Script.sh

--- SVG_PNG_Script.sh
+++ SVG_PNG_Script.sh
@@ -7,7 +7,7 @@ do
 	rsvg-convert -b white -o weather-script-output.png TEST.svg
 	pngcrush -c 0 -ow weather-script-output.png
 	sudo cp -f weather-script-output.png /var/www/html/
-	/home/pi/Downloads/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.png /Programing/
-	/home/pi/Downloads/Dropbox-Uploader/dropbox_uploader.sh upload TEST.svg /Programing/
+	/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.png /Programing/
+	/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload TEST.svg /Programing/
 	sleep 20
 done
