d36520f71924e3d6aea7ce00272bc66879423bb9
worriedmedic <comical115@gmail.com>
2016-08-07 14:21:15 -0400

    new file:   cutty_tweeter.py 	new file:   cutty_tweeter.sh

---
+++ cutty_tweeter.py
@@ -0,0 +1,64 @@
+# !/bin/python
+# Created from https://www.mathworks.com/help/thingspeak/thingtweet-app.html
+# Using requests
+
+import os
+import time
+import datetime
+import requests
+import os.path
+
+url     = 'https://api.thingspeak.com/apps/thingtweet/1/statuses/update'
+#delay   = 600 #In Seconds ### REDUNDANT - NOW USING CRONTAB
+api_key = 'UERV67G8O01HXYVV' #Key for the ThingSpeak Tweeter API
+logging = 'false'
+verbose = 'false'
+
+dover_sensor = { #Modified for Dover_Wx_St
+    'Inside'        : '123799',
+    'Outside'       : '123694',
+    'Downstairs'    : '124921',
+    'Garage'        : '125305',
+    'Attic'         : '130195'
+    
+}
+
+def thingspeaktweet(api_key):
+    tweet = "Outside: %%channel_123694_field_2%%F, %%channel_123694_field_4%%%, Inside: %%channel_123799_field_2%%F, %%channel_123799_field_4%%%, Downstairs: %%channel_124921_field_2%%F, %%channel_124921_field_4%%%, Garage: %%channel_125305_field_2%%F, %%channel_125305_field_4%%%, Attic: %%channel_130195_field_2%%F, %%channel_130195_field_4%%%, Press: %%channel_123694_field_3%%mb"
+    if verbose == 'true':
+        print(tweet)
+    payload = {'api_key': api_key, 'status': tweet}
+    try:
+        r = requests.post(url, data=payload)
+        time.sleep(1)
+        if verbose == 'true':
+            print(str(today), str(now), r.text)
+            
+    except requests.exceptions.RequestException as e:
+        print("TWEETAPI REQUESTS ERROR", today, now, e)
+    except Exception as e:
+        print("TWEETAPI GENERIC ERROR", today, now, e)
+        
+    if logging == 'true':
+        try:
+            fname = str(today) + '.log'
+            fdirectory = 'data_log'
+            fmode = 'a' #Append
+
+            if not os.path.exists(fdirectory):
+                os.makedirs(fdirectory)
+   
+            log = today + ',' + now + ',' + r + '\n'
+        
+            outf = open(os.path.join(fdirectory, fname), fmode)
+            outf.write(log)
+            outf.flush()
+        except Exception as e:
+            print("DATA LOG ERROR", today, now, e)
+
+try:
+    now = time.strftime("%H:%M:%S")
+    today = str(datetime.date.today())
+    thingspeaktweet(api_key)
+except Exception as e:
+    print("GENERIC ERROR", today, now, e)
