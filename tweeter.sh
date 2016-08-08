275a52ed89d4e9d69ad7f05e1f89aa38bafc4e04
worriedmedic <comical115@gmail.com>
2016-08-07 21:17:36 -0400

    Update tweeter.sh

--- tweeter.sh
+++ tweeter.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-sleep 2
 cd /home/pi/Power_Monitoring
-python tweeter.py > /home/pi/Power_Monitoring/data_log/tweeter.log 2>&1
+python tweeter.py >> /home/pi/Power_Monitoring/data_log/tweeter.log 2>&1
