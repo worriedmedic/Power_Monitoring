81cb7be097b1cc9e926c352d596e6b2cf15adbfb
worriedmedic <comical115@gmail.com>
2016-08-07 21:27:34 -0400

    Update data_log_update.sh

--- Dropbox-Uploader/data_log_update.sh
+++ Dropbox-Uploader/data_log_update.sh
@@ -4,5 +4,9 @@
 /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload ~/Power_Monitoring/data_log/ /Programming/
 /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload /var/log/network_restart.log /Programming/data_log/
 
+if [ ! -d "/home/pi/Power_Monitoring/data_archive" ]; then
+  mkdir /home/pi/Power_Monitoring/data_archive
+fi
+
 find /home/pi/Power_Monitoring/data_log/* -mtime +7 -exec cp {} /home/pi/Power_Monitoring/data_archive/  \; #Copies any files older than 7 days old to an archive location
 find /home/pi/Power_Monitoring/data_log/* -mtime +7 -delete #Deletes files older than 7 days to prevent extensive dropbox uploads
