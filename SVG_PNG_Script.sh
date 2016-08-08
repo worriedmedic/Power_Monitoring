68880ed3dba61dfa32e2eefe59529ecc9ec451f3
worriedmedic <comical115@gmail.com>
2016-08-07 21:02:47 -0400

    Update SVG_PNG_Script.sh

--- SVG_PNG_Script.sh
+++ SVG_PNG_Script.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+#Script converts SVG output to a PNG and then converts it properly so it can be displayed on the kindle
+#Also copies the SVG and PNG file to dropbox
 
 cd /home/pi/Power_Monitoring
 
@@ -8,7 +10,7 @@ do
 	pngcrush -c 0 -ow weather-script-output.png
 	sudo cp -f weather-script-output.png /var/www/html/
 	sudo chmod 755 /var/www/html/weather-script-output.png
-	/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.png /Programing/
-	/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload TEST.svg /Programing/
+	/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.png /Programming/
+	/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload TEST.svg /Programming/
 	sleep 20
 done
