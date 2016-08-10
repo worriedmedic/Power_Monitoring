a7263c133c580a46b4c11db8d847d4ffd4b3494e
worriedmedic <comical115@gmail.com>
2016-08-10 10:33:10 -0400

    Update SVG_Processing.sh

--- SVG_Processing.sh
+++ SVG_Processing.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [ ! -d "/home/pi/Power_Monitoring/data_log" ]; then
+  mkdir /home/pi/Power_Monitoring/data_log
+fi
+
 cd /home/pi/Power_Monitoring
 python SVG_Processing.py >> /home/pi/Power_Monitoring/data_log/SVG_Processing.log 2>&1
