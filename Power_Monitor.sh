a4660564721768d76e281bfd1c42769ed3087e5c
worriedmedic <comical115@gmail.com>
2016-08-10 10:32:49 -0400

    Update Power_Monitor.sh

--- Power_Monitor.sh
+++ Power_Monitor.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [ ! -d "/home/pi/Power_Monitoring/data_log" ]; then
+  mkdir /home/pi/Power_Monitoring/data_log
+fi
+
 cd /home/pi/Power_Monitoring
 python Power_Monitor.py >> /home/pi/Power_Monitoring/data_log/Power_Monitor.log 2>&1
