dc16c468ab9ed4ec13fa770403a61264615fbed7
worriedmedic <comical115@gmail.com>
2016-08-10 01:10:46 -0400

    Update data_log_update.sh

--- Dropbox-Uploader/data_log_update.sh
+++ Dropbox-Uploader/data_log_update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #Script to update log files to dropbox
 
-if [ -f '~/Power_Monitoring/dover.location' ]; then
+if [ -f 'home/pi/Power_Monitoring/dover.location' ]; then
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload ~/Power_Monitoring/data_log/ /Programming/dover/
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload /var/log/network_restart.log /Programming/dover/data_log/
-elif [ -f '~/Power_Monitoring/cuttyhunk.location' ]; then
+elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload ~/Power_Monitoring/data_log/ /Programming/
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload /var/log/network_restart.log /Programming/data_log/
 fi
