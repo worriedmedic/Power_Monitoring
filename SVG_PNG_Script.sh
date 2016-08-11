722455ce51983b0a36eca81f1457dad6b47d88e9
worriedmedic <comical115@gmail.com>
2016-08-11 09:27:30 -0400

    Update SVG_PNG_Script.sh

--- SVG_PNG_Script.sh
+++ SVG_PNG_Script.sh
@@ -2,20 +2,20 @@
 #Script converts SVG output to a PNG and then converts it properly so it can be displayed on the kindle
 #Also copies the SVG and PNG file to dropbox
 
-cd /home/pi/Power_Monitoring
+cd /home/pi/Power_Monitoring/output
 
 while :
 do
-	rsvg-convert -b white -o weather-script-output.png TEST.svg
+	rsvg-convert -b white -o weather-script-output.png weather-script-output.svg
 	pngcrush -c 0 -ow weather-script-output.png
 	sudo cp -f weather-script-output.png /var/www/html/
 	sudo chmod 755 /var/www/html/weather-script-output.png
 	if [ -f 'dover.location' ]; then
 		/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.png /Programming/dover/
-		/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload TEST.svg /Programming/dover/
+		/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.svg /Programming/dover/
 	elif [ -f 'cuttyhunk.location' ]; then
 		/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.png /Programming/
-		/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload TEST.svg /Programming/
+		/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.svg /Programming/
 	fi
 
 	sleep 10
