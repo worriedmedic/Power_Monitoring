e0427001b78ee9589d9a2d321326c601f5185590
worriedmedic <comical115@gmail.com>
2016-08-11 10:37:47 -0400

    Update SVG_PNG_Script.sh

--- SVG_PNG_Script.sh
+++ SVG_PNG_Script.sh
@@ -10,10 +10,10 @@ do
 	pngcrush -c 0 -ow weather-script-output.png
 	sudo cp -f weather-script-output.png /var/www/html/
 	sudo chmod 755 /var/www/html/weather-script-output.png
-	if [ -f 'dover.location' ]; then
+	if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
 		/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.png /Programming/dover/
 		/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.svg /Programming/dover/
-	elif [ -f 'cuttyhunk.location' ]; then
+	elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
 		/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.png /Programming/data_log/
 		/home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh upload weather-script-output.svg /Programming/data_log/
 	fi
