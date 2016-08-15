54137e314516070f266645d72b4a63e617b8e58c
worriedmedic <comical115@gmail.com>
2016-08-15 10:16:25 -0400

    Update Power_Monitor.sh

--- Power_Monitor.sh
+++ Power_Monitor.sh
@@ -5,4 +5,4 @@ if [ ! -d "/home/pi/Power_Monitoring/data_log" ]; then
 fi
 
 cd /home/pi/Power_Monitoring
-python Power_Monitor.py >> /home/pi/Power_Monitoring/data_log/Power_Monitor.log
+python Power_Monitor.py >> /home/pi/Power_Monitoring/data_log/Power_Monitor.log 2>&1
