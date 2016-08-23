804c9ce411ee2e4eada03ee73a8a9f85436d7418
worriedmedic <comical115@gmail.com>
2016-08-23 08:52:27 -0400

    Update data_log_update.sh

--- Dropbox-Uploader/data_log_update.sh
+++ Dropbox-Uploader/data_log_update.sh
@@ -9,7 +9,7 @@ if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload ~/Power_Monitoring/data_log/ /Programming/dover/
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload /var/log/network_restart.log /Programming/dover/data_log/
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload ~/.ngrok2/_ngrok.log /Programming/dover/data_log/sys_log/
-  /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload /var/log/fail2bain.log /Programming/dover/data_log/sys_log/
+  /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload /var/log/fail2ban.log /Programming/dover/data_log/sys_log/
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload ~/.vnc/raspberrypi\:1.log /Programming/dover/data_log/sys_log/
   
   find /home/pi/Power_Monitoring/data_log/* -mtime +2 -exec cp {} /home/pi/Power_Monitoring/data_archive/  \; #Copies any files older than 7 days old to an archive location
@@ -20,7 +20,7 @@ elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload ~/Power_Monitoring/data_log/ /Programming/
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload /var/log/network_restart.log /Programming/data_log/
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload ~/.ngrok2/_ngrok.log /Programming/data_log/
-  /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload /var/log/fail2bain.log /Programming/dover/data_log/sys_log/
+  /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload /var/log/fail2ban.log /Programming/dover/data_log/sys_log/
   /home/pi/Power_Monitoring/Dropbox-Uploader/dropbox_uploader.sh -p upload ~/.vnc/raspberrypi\:1.log /Programming/dover/data_log/sys_log/
   
   find /home/pi/Power_Monitoring/data_log/* -mtime +7 -exec cp {} /home/pi/Power_Monitoring/data_archive/  \; #Copies any files older than 7 days old to an archive location
