4ab73ae58bb4955a2bbf4c1738f23ddfc7ad872c
worriedmedic <comical115@gmail.com>
2016-09-09 12:16:36 -0400

    Update pandas_test.py

--- plotting/pandas_test.py
+++ plotting/pandas_test.py
@@ -16,6 +16,6 @@ s2 = s.loc[s['Address'] == 2]
 s3 = s.loc[s['Address'] == 3]
 s4 = s.loc[s['Address'] == 4]
 
-plot = plt.plot([s0['Time'], s0['Temperature']])
+plot = plt.plot(s0['Time'].values, s0['Temperature'].values)
 fig = plot.get_figure()
 fig.savefig("output.png")
