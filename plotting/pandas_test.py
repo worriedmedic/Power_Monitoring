c4c93abaf3a05423cc802ecef3c80760cefdc029
worriedmedic <comical115@gmail.com>
2016-09-09 15:31:28 -0400

    Update pandas_test.py

--- plotting/pandas_test.py
+++ plotting/pandas_test.py
@@ -9,7 +9,8 @@ s['Pressure'] = s['Pressure'].map(lambda x: x.lstrip('P'))
 s['Humidity'] = s['Humidity'].map(lambda x: x.lstrip('H'))
 s['Voltage'] = s['Voltage'].map(lambda x: x.lstrip('V'))
 
-#s = s.drop('Date', 1)
+s.drop('Date', 1)
+s['Time'] = pd.to_datetime(s['Time'])
 
 s0 = s.loc[s['Address'] == 0]
 s1 = s.loc[s['Address'] == 1]
@@ -19,25 +20,21 @@ s4 = s.loc[s['Address'] == 4]
 
 plt.figure()
 
-dates = matplotlib.dates.date2num(s0['Time'])
+#dates = matplotlib.dates.date2num(s0['Time'])
 #plot_date(dates, values)
 
-plt.subplot(211)
-s0temp = plt.plot(dates, s0['Temperature'].values.astype(float))
-plt.gcf().autofmt_xdate()
-#s0temp = plt.plot_date(dates, s0['Temperature'].values.astype(float))
-plt.setp(s0temp, color='r', linewidth=2.0)
-plt.title('Ext Sensor: Temp')
-plt.xlabel('Time')
-plt.ylabel('Temp')
+fig, ax1 = plt.subplot(211)
+ax1 = plot_dates(s0['Time'], s0['Temperature'].values.astype(float))
+ax1.setp(ax1, color='r', linewidth=2.0)
+ax1.title('Ext Sensor: Temp')
+ax1.xlabel('Time')
+ax1.ylabel('Temp')
 
-plt.subplot(212)
-s0press = plt.plot(dates, s0['Pressure'].values.astype(float))
-plt.gcf().autofmt_xdate()
-#s0press = plt.plot_date(dates, s0['Pressure'].values.astype(float))
-plt.setp(s0press, color='r', linewidth=2.0)
-plt.xlabel('Time')
-plt.ylabel('Press')
+fig, ax2 = plt.subplot(212)
+ax2 = plot_dates(s0['Time'], s0['Pressure'].values.astype(float))
+ax2.setp(ax2, color='r', linewidth=2.0)
+ax2.xlabel('Time')
+ax2.ylabel('Press')
 
 
 plt.savefig("output.png")
