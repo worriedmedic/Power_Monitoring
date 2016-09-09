3a84c69761a1d41dbfa225d6bbbda474fd21a545
worriedmedic <comical115@gmail.com>
2016-09-09 14:48:50 -0400

    Update pandas_test.py

--- plotting/pandas_test.py
+++ plotting/pandas_test.py
@@ -16,8 +16,26 @@ s2 = s.loc[s['Address'] == 2]
 s3 = s.loc[s['Address'] == 3]
 s4 = s.loc[s['Address'] == 4]
 
-plt.figure(1)
-plt.plot(pd.to_datetime(s0['Time']), s0['Temperature'].values.astype(float), 'bo')
+fig = plt.figure()
+
+ax1 = fig.add_subplot(211)
+plt.plot(pd.to_datetime(s0['Time']), s0['Temperature'].values.astype(float))
+plt.setp(lines, color='r', linewidth=2.0)
+plt.title('Ext Sensor: Temp')
+plt.xlabel('Time')
+plt.ylabel('Temp')
+plt.axis('scaled')
+
+ax2 = fig.add_subplot(212, sharex=ax1, sharey=ax1)
+
+#plt.axis([0,12,60,100])
+plt.subplot(222, sharex=ax1, sharey=ax1)
+plt.plot(pd.to_datetime(s0['Time']), s0['Pressure'].values.astype(float))
+plt.setp(lines, color='r', linewidth=2.0)
+plt.xlabel('Time')
+plt.ylabel('Temp')
+plt.axis('scaled')
+
 
 plt.savefig("output.png")
 plt.show()
