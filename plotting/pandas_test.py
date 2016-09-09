b90d96ef885d5ebfb7d8fbcf0f77a8af611e37fd
worriedmedic <comical115@gmail.com>
2016-09-09 11:01:39 -0400

    Create pandas_test.py

---
+++ plotting/pandas_test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+s = pd.read_csv('/home/pi/Power_Monitoring/data_log/2016-09/2016-09-09.log', names = ["Date", "Time", "Address", "Temperature", "Pressure", "Humidity", "Voltage", "RSSI"])
+s['Temperature'] = s['Temperature'].map(lambda x: x.lstrip('T'))
+s['Pressure'] = s['Pressure'].map(lambda x: x.lstrip('P'))
+s['Humidity'] = s['Humidity'].map(lambda x: x.lstrip('H'))
+s['Voltage'] = s['Voltage'].map(lambda x: x.lstrip('V'))
+
+s.sort(["Address","Time"])
