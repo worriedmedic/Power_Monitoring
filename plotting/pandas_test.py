915a2d7df2ab71cb5a27ab2906c7cb5d0bf77377
worriedmedic <comical115@gmail.com>
2016-09-10 13:31:39 -0400

    Update pandas_test.py

--- plotting/pandas_test.py
+++ plotting/pandas_test.py
@@ -2,40 +2,58 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import time, datetime
+import traceback
 
-s = pd.read_csv('/home/pi/Power_Monitoring/data_log/2016-09/2016-09-09.log', names = ["Date", "Time", "Address", "Temperature", "Pressure", "Humidity", "Voltage", "RSSI"])
-s['Temperature'] = s['Temperature'].map(lambda x: x.lstrip('T'))
-s['Pressure'] = s['Pressure'].map(lambda x: x.lstrip('P'))
-s['Humidity'] = s['Humidity'].map(lambda x: x.lstrip('H'))
-s['Voltage'] = s['Voltage'].map(lambda x: x.lstrip('V'))
+if (1):
+  if (today != datetime.date.today()):
+    today = datetime.date.today()
+    yesterday = datetime.date.today() + datetime.timedelta(days=-1)
+    fname = str(today) + '.log'
+    fdirectory = 'data_log/' + time.strftime("%Y-%m")
 
-s = s.drop('Date', 1)
-s['Time'] = pd.to_datetime(s['Time'])
+    try:
+      data_yest = data_today
+      data_yest0 = data_today0
+      data_yest1 = data_today1
+      data_yest2 = data_today2
+      data_yest3 = data_today3
+      data_yest4 = data_today4
+    except exception:
+      print("TODAY TO YESTERDAY DATA XFER")
+			traceback.print_exc(file=sys.stdout)
+			print('-' * 60)
+      
+    try:
+      data_today = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + time.strftime("%Y-%m") + '/' + str(today) + '.log', names = ["Date", "Time", "Address", "Temperature", "Pressure", "Humidity", "Voltage", "RSSI"])
+      data_today['Temperature'] = s['Temperature'].map(lambda x: x.lstrip('T'))
+      data_today['Pressure'] = s['Pressure'].map(lambda x: x.lstrip('P'))
+      data_today['Humidity'] = s['Humidity'].map(lambda x: x.lstrip('H'))
+      data_today['Voltage'] = s['Voltage'].map(lambda x: x.lstrip('V'))
 
-s0 = s.loc[s['Address'] == 0]
-s1 = s.loc[s['Address'] == 1]
-s2 = s.loc[s['Address'] == 2]
-s3 = s.loc[s['Address'] == 3]
-s4 = s.loc[s['Address'] == 4]
+      data_today['Datetime'] = pd.to_datetime(data_today['Date'] + ' ' + data_today['Time'])
+      data_today.setindex('Datetime')
+      data_today.drop(['Date', 'Time'], 1)
 
-plt.figure()
+      data_today0 = data_today.loc[s['Address'] == 0]
+      data_today1 = data_today.loc[s['Address'] == 1]
+      data_today2 = data_today.loc[s['Address'] == 2]
+      data_today3 = data_today.loc[s['Address'] == 3]
+      data_today4 = data_today.loc[s['Address'] == 4]
+      
+    except Exception:
+      print("READ CSV ERROR")
+			traceback.print_exc(file=sys.stdout)
+			print('-' * 60)
 
-#dates = matplotlib.dates.date2num(s0['Time'])
-#plot_date(dates, values)
-
-fig, ax1 = plt.subplot(211)
-ax1 = plt.plot_date(s0['Time'], s0['Temperature'].values.astype(float))
-ax1.setp(ax1, color='r', linewidth=2.0)
-ax1.title('Ext Sensor: Temp')
-ax1.xlabel('Time')
-ax1.ylabel('Temp')
-
-fig, ax2 = plt.subplot(212)
-ax2 = plot_dates(s0['Time'], s0['Pressure'].values.astype(float))
-ax2.setp(ax2, color='r', linewidth=2.0)
-ax2.xlabel('Time')
-ax2.ylabel('Press')
-
-
-plt.savefig("output.png")
-plt.show()
+    try:
+      data_today
+      data_today0
+      data_today1
+      data_today2
+      data_today3
+      data_today4
+    except Exception:
+      print("READ CSV ERROR")
+			traceback.print_exc(file=sys.stdout)
+			print('-' * 60) 
