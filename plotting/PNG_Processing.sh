8d5729e218083ba87199e031484c2f41542cb2e3
worriedmedic <comical115@gmail.com>
2016-09-11 00:40:01 -0400

    Update PNG_Processing.sh

--- plotting/PNG_Processing.sh
+++ plotting/PNG_Processing.sh
@@ -4,14 +4,14 @@ sleep 20 #Give the python script time to output the images...
 
 cd /home/pi/Power_Monitoring/output
 
-pngcrush -c 0 -ow plot_temp.png
-pngcrush -c 0 -ow plot_press.png
-pngcrush -c 0 -ow plot_humid.png
-sudo cp -f plot_temp.png plot_press.png plot_humid.png /var/www/html/
-sudo chmod 755 /var/www/html/plot_temp.png /var/www/html/plot_press.png /var/www/html/plot_humid.png
+pngcrush -c 0 -ow plot_temp_16.png
+pngcrush -c 0 -ow plot_press_16.png
+pngcrush -c 0 -ow plot_humid_16.png
+sudo cp -f plot_temp_16.png plot_press_16.png plot_humid_16.png /var/www/html/
+sudo chmod 755 /var/www/html/plot_temp_16.png /var/www/html/plot_press_16.png /var/www/html/plot_humid_16.png
 
 if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
-  /usr/local/bin/dropbox_uploader.sh -p upload plot_temp.png plot_press.png plot_humid.png /Programming/logs/cuttyhunk/plots/
+  /usr/local/bin/dropbox_uploader.sh -p upload plot_temp_16.png plot_press_16.png plot_humid_16.png /Programming/logs/cuttyhunk/plots/
 elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
-  /usr/local/bin/dropbox_uploader.sh -p upload plot_temp.png plot_press.png plot_humid.png /Programming/logs/cuttyhunk/plots/
+  /usr/local/bin/dropbox_uploader.sh -p upload plot_temp_16.png plot_press_16.png plot_humid_16.png /Programming/logs/cuttyhunk/plots/
 fi
