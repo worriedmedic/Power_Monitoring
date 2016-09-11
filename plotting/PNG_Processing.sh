9ea23dd5233b055f3de2de625f9b277310376502
worriedmedic <comical115@gmail.com>
2016-09-10 23:59:48 -0400

    Update PNG_Processing.sh

--- plotting/PNG_Processing.sh
+++ plotting/PNG_Processing.sh
@@ -7,3 +7,5 @@ pngcrush -c 0 -ow plot_press.png
 pngcrush -c 0 -ow plot_humid.png
 sudo cp -f plot_temp.png plot_press.png plot_humid.png /var/www/html/
 sudo chmod 755 /var/www/html/plot_temp.png /var/www/html/plot_press.png /var/www/html/plot_humid.png
+
+/usr/local/bin/dropbox_uploader.sh upload -p plot_temp.png plot_press.png plot_humid.png /Programming/logs/cuttyhunk/
