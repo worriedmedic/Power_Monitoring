8abe9334c54f5a16f222029f587a294cd81629e8
worriedmedic <comical115@gmail.com>
2016-09-10 20:15:31 -0400

    Update PNG_Processing.sh

--- plotting/PNG_Processing.sh
+++ plotting/PNG_Processing.sh
@@ -2,6 +2,6 @@
 
 cd /home/pi/Power_Monitoring/output
 
-pngcrush -c 0 -ow 00_01_Temperature.png
-sudo cp -f 00_01_Temperature.png /var/www/html/
-sudo chmod 755 /var/www/html/00_01_Temperature.png
+pngcrush -c 0 -ow 00_01_Temp.png
+sudo cp -f 00_01_Temp.png /var/www/html/
+sudo chmod 755 /var/www/html/00_01_Temp.png
