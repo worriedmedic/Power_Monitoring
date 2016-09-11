dc59887a294d6742f672834cb6cba11945be1e76
worriedmedic <comical115@gmail.com>
2016-09-10 21:21:46 -0400

    Update PNG_Processing.sh

--- plotting/PNG_Processing.sh
+++ plotting/PNG_Processing.sh
@@ -2,6 +2,8 @@
 
 cd /home/pi/Power_Monitoring/output
 
-pngcrush -c 0 -ow 00_01_Temp.png
-sudo cp -f 00_01_Temp.png /var/www/html/
-sudo chmod 755 /var/www/html/00_01_Temp.png
+pngcrush -c 0 -ow plot_temp.png
+pngcrush -c 0 -ow plot_press.png
+pngcrush -c 0 -ow plot_humid.png
+sudo cp -f plot_temp.png plot_press.png plot_humid.png /var/www/html/
+sudo chmod 755 /var/www/html/plot_temp.png /var/www/html/plot_press.png /var/www/html/plot_humid.png
