48b5d51749c6389202fa35d5ec9bd595b0df87f5
worriedmedic <comical115@gmail.com>
2016-09-11 00:27:12 -0400

    Update PNG_Processing.sh

--- plotting/PNG_Processing.sh
+++ plotting/PNG_Processing.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 20 #Give the python script time to output the images...
+
 cd /home/pi/Power_Monitoring/output
 
 pngcrush -c 0 -ow plot_temp.png
@@ -8,4 +10,8 @@ pngcrush -c 0 -ow plot_humid.png
 sudo cp -f plot_temp.png plot_press.png plot_humid.png /var/www/html/
 sudo chmod 755 /var/www/html/plot_temp.png /var/www/html/plot_press.png /var/www/html/plot_humid.png
 
-/usr/local/bin/dropbox_uploader.sh -p upload plot_temp.png plot_press.png plot_humid.png /Programming/logs/cuttyhunk/
+if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
+  /usr/local/bin/dropbox_uploader.sh -p upload plot_temp.png plot_press.png plot_humid.png /Programming/logs/cuttyhunk/plots/
+elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
+  /usr/local/bin/dropbox_uploader.sh -p upload plot_temp.png plot_press.png plot_humid.png /Programming/logs/cuttyhunk/plots/
+fi
