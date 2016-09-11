d4a4fe7499c98ea0eafaa9e9cf6b74ed141ee8b0
worriedmedic <comical115@gmail.com>
2016-09-10 21:10:43 -0400

    Update pandas_test_1.py

--- plotting/pandas_test_1.py
+++ plotting/pandas_test_1.py
@@ -6,6 +6,10 @@ import time, datetime
 import traceback
 import sys
 
+temp_plot = True
+press_plot = True
+humid_plot = True
+
 if (1):
 	today = datetime.date.today()
 	yesterday = datetime.date.today() + datetime.timedelta(days=-1)
@@ -14,20 +18,25 @@ if (1):
 	
 	try:
 		data_today = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + time.strftime("%Y-%m") + '/' + str(today) + '.log', names = ["Date", "Time", "Address", "Temperature", "Pressure", "Humidity", "Voltage", "RSSI"], dtype=str)
-		data_today['Temperature'] = data_today['Temperature'].str.replace('T', '')
-		data_today['Pressure'] = data_today['Pressure'].str.replace('P', '')
-		data_today['Humidity'] = data_today['Humidity'].str.replace('H', '')
-		data_today['Voltage'] = data_today['Voltage'].str.replace('V', '')
-		data_today['Datetime'] = pd.to_datetime(data_today['Date'] + ' ' + data_today['Time'])
-
-		data_today = data_today.drop(['Date', 'Time'], 1)
-		data_today = data_today.set_index('Datetime')
-
-		data_today0 = data_today.loc[data_today['Address'] == '00']
-		data_today1 = data_today.loc[data_today['Address'] == '01']
-		data_today2 = data_today.loc[data_today['Address'] == '02']
-		data_today3 = data_today.loc[data_today['Address'] == '03']
-		data_today4 = data_today.loc[data_today['Address'] == '04']
+		data_yest = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + time.strftime("%Y-%m") + '/' + str(yesterday) + '.log', names = ["Date", "Time", "Address", "Temperature", "Pressure", "Humidity", "Voltage", "RSSI"], dtype=str)
+		
+		data = pd.concat([data_yest, data_today])
+		
+		data['Temperature'] = data['Temperature'].str.replace('T', '')
+		data['Pressure'] = data['Pressure'].str.replace('P', '')
+		data['Humidity'] = data['Humidity'].str.replace('H', '')
+		data['Voltage'] = data['Voltage'].str.replace('V', '')
+		
+		data['Datetime'] = pd.to_datetime(data['Date'] + ' ' + data['Time'])
+		
+		data = data.drop(['Date', 'Time'], 1)
+		data = data.set_index('Datetime')
+		
+		data0 = data.loc[data['Address'] == '00']
+		data1 = data.loc[data['Address'] == '01']
+		data2 = data.loc[data['Address'] == '02']
+		data3 = data.loc[data['Address'] == '03']
+		data4 = data.loc[data['Address'] == '04']
 
 	except Exception:
 		print("TODAY READ CSV ERROR")
@@ -35,52 +44,60 @@ if (1):
 		print('-' * 60)
 
 	try:
-		data_yest = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + time.strftime("%Y-%m") + '/' + str(yesterday) + '.log', names = ["Date", "Time", "Address", "Temperature", "Pressure", "Humidity", "Voltage", "RSSI"], dtype=str)
-		data_yest['Temperature'] = data_yest['Temperature'].str.replace('T', '')
-		data_yest['Pressure'] = data_yest['Pressure'].str.replace('P', '')
-		data_yest['Humidity'] = data_yest['Humidity'].str.replace('H', '')
-		data_yest['Voltage'] = data_yest['Voltage'].str.replace('V', '')
-		data_yest['Datetime'] = pd.to_datetime(data_yest['Date'] + ' ' + data_yest['Time'])
+		if temp_plot:
+			fig = plt.figure(figsize=(6, 8))
 
-		data_yest = data_yest.drop(['Date', 'Time'], 1)
-		data_yest = data_yest.set_index('Datetime')
+			plt.style.use('fivethirtyeight')
 
-		data_yest0 = data_yest.loc[data_yest['Address'] == '00']
-		data_yest1 = data_yest.loc[data_yest['Address'] == '01']
-		data_yest2 = data_yest.loc[data_yest['Address'] == '02']
-		data_yest3 = data_yest.loc[data_yest['Address'] == '03']
-		data_yest4 = data_yest.loc[data_yest['Address'] == '04']
+			plt.plot_date(data0.between_time(now_minus_eight, now).index, data0['Temperature'].between_time(now_minus_eight, now).values, label="Sensor 00")
+			plt.plot_date(data1.between_time(now_minus_eight, now).index, data1['Temperature'].between_time(now_minus_eight, now).values, label="Sensor 01")
+			plt.plot_date(data2.between_time(now_minus_eight, now).index, data2['Temperature'].between_time(now_minus_eight, now).values, label="Sensor 02")
+			plt.plot_date(data4.between_time(now_minus_eight, now).index, data4['Temperature'].between_time(now_minus_eight, now).values, label="Sensor 04")
+			plt.legend(loc=0)
+			plt.title('Temperature Plot: Past 8 Hours')
+			plt.xlabel('Time')
+			plt.ylabel('Temp (F)')
+			plt.grid()
+			plt.tight_layout()
+			fig.autofmt_xdate()
+			fig.savefig('/home/pi/Power_Monitoring/output/plot_temp.png', facecolor=fig.get_facecolor(), edgecolor='none', bbox_inches='tight')
+		
+		if press_plot:
+			fig = plt.figure(figsize=(6, 8))
 
-	except Exception:
-		print("YESTERDAY READ CSV ERROR")
-		traceback.print_exc(file=sys.stdout)
-		print('-' * 60)
+			plt.style.use('fivethirtyeight')
 
-	try:
-		print(data_today0.between_time(now_minus_eight, now))
-		#print(data_yest4.between_time('12:00','23:59'))
+			plt.plot_date(data0.between_time(now_minus_eight, now).index, data0['Pressure'].between_time(now_minus_eight, now).values, label="Sensor 00")
+			plt.plot_date(data1.between_time(now_minus_eight, now).index, data1['Pressure'].between_time(now_minus_eight, now).values, label="Sensor 01")
+			plt.plot_date(data2.between_time(now_minus_eight, now).index, data2['Pressure'].between_time(now_minus_eight, now).values, label="Sensor 02")
+			plt.plot_date(data4.between_time(now_minus_eight, now).index, data4['Pressure'].between_time(now_minus_eight, now).values, label="Sensor 04")
+			plt.legend(loc=0)
+			plt.title('Pressure Plot: Past 8 Hours')
+			plt.xlabel('Time')
+			plt.ylabel('Pressure (hPa)')
+			plt.grid()
+			plt.tight_layout()
+			fig.autofmt_xdate()
+			fig.savefig('/home/pi/Power_Monitoring/output/plot_press.png', facecolor=fig.get_facecolor(), edgecolor='none', bbox_inches='tight')
+			
+		if humid_plot:
+			fig = plt.figure(figsize=(6, 8))
 
-	except Exception:
-		print("PRINT DATA ERROR")
-		traceback.print_exc(file=sys.stdout)
-		print('-' * 60) 
+			plt.style.use('fivethirtyeight')
 
-	try:
-		fig = plt.figure(figsize=(6, 8))
-
-		plt.style.use('fivethirtyeight')
-
-		plt.plot_date(data_today0.between_time(now_minus_eight, now).index, data_today0['Temperature'].between_time(now_minus_eight, now).values, linestyle="solid", marker='.', label="Sensor 00")
-		plt.plot_date(data_today1.between_time(now_minus_eight, now).index, data_today1['Temperature'].between_time(now_minus_eight, now).values, linestyle="solid", marker='.', label="Sensor 01")
-		plt.legend(loc=0)
-		plt.title('Ext Sensor: Temp')
-		plt.xlabel('Time')
-		plt.ylabel('Temp')
-		plt.grid()
-		plt.tight_layout()
-		fig.autofmt_xdate()
-		fig.savefig('/home/pi/Power_Monitoring/output/00_01_Temp.png', bbox_inches='tight')
-		#plt.show()
+			plt.plot_date(data0.between_time(now_minus_eight, now).index, data0['Humidity'].between_time(now_minus_eight, now).values, label="Sensor 00")
+			plt.plot_date(data1.between_time(now_minus_eight, now).index, data1['Humidity'].between_time(now_minus_eight, now).values, label="Sensor 01")
+			plt.plot_date(data2.between_time(now_minus_eight, now).index, data2['Humidity'].between_time(now_minus_eight, now).values, label="Sensor 02")
+			plt.plot_date(data4.between_time(now_minus_eight, now).index, data4['Humidity'].between_time(now_minus_eight, now).values, label="Sensor 04")
+			plt.legend(loc=0)
+			plt.title('Pressure Plot: Past 8 Hours')
+			plt.xlabel('Time')
+			plt.ylabel('Humid (%)')
+			plt.grid()
+			plt.tight_layout()
+			fig.autofmt_xdate()
+			fig.savefig('/home/pi/Power_Monitoring/output/plot_press.png', facecolor=fig.get_facecolor(), edgecolor='none', bbox_inches='tight')
+			
 	except Exception:
 		print("MATPLOTLIB ERROR")
 		traceback.print_exc(file=sys.stdout)
