665398db50a221d9a09b8b28f416814debf867bc
worriedmedic <comical115@gmail.com>
2016-09-11 21:32:48 -0400

    Update and rename 16_Hour_Plot.py to Matplotlib_plot.py

---
+++ plotting/Matplotlib_plot.py
@@ -0,0 +1,136 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import time, datetime
+import traceback
+import sys, os.path
+
+temp_plot = True
+press_plot = True
+humid_plot = True
+
+td = '16H'
+
+line_width = 4
+marker_size = 6
+
+if os.path.isfile('dover.location'):
+	plt_size     = figsize=(10, 8), dpi=84
+	sensor0      = '09'
+	sensor0label = 'Outside'
+	sensor1      = '08'
+	sensor1label = 'Upstairs'
+	sensor2      = '07'
+	sensor2label = 'Downstairs'
+	sensor3      = '06'
+	sensor3label = 'Garage'
+	sensor4      = '05'
+	sensor4label = 'Attic'
+elif os.path.isfile('cuttyhunk.location'):
+	plt_size     = figsize=(6.5, 9), dpi=84
+	sensor0      = '00'
+	sensor0label = 'Outside'
+	sensor1      = '01'
+	sensor1label = 'Upstairs'
+	sensor2      = '02'
+	sensor2label = 'Downstairs'
+	sensor3      = '03'
+	sensor3label = 'Barn Upstairs'
+	sensor4      = '04'
+	sensor4label = 'Reeds Room'
+
+
+if (1):
+	today = datetime.date.today()
+	yesterday = datetime.date.today() + datetime.timedelta(days=-1)
+	now = datetime.datetime.now()
+	now_minus_eight = now + datetime.timedelta(hours=-8)
+	
+	try:
+		data_today = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + time.strftime("%Y-%m") + '/' + str(today) + '.log', names = ["Date", "Time", "Address", "Temperature", "Pressure", "Humidity", "Voltage", "RSSI"], dtype=str)
+		data_yest = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + time.strftime("%Y-%m") + '/' + str(yesterday) + '.log', names = ["Date", "Time", "Address", "Temperature", "Pressure", "Humidity", "Voltage", "RSSI"], dtype=str)
+		
+		data = pd.concat([data_yest, data_today])
+		
+		data['Temperature'] = data['Temperature'].str.replace('T', '')
+		data['Pressure'] = data['Pressure'].str.replace('P', '')
+		data['Humidity'] = data['Humidity'].str.replace('H', '')
+		data['Voltage'] = data['Voltage'].str.replace('V', '')
+		
+		data['Datetime'] = pd.to_datetime(data['Date'] + ' ' + data['Time'])
+		
+		data = data.drop(['Date', 'Time'], 1)
+		data = data.set_index('Datetime')
+		
+		data0 = data.loc[data['Address'] == sensor0]
+		data1 = data.loc[data['Address'] == sensor1]
+		data2 = data.loc[data['Address'] == sensor2]
+		data3 = data.loc[data['Address'] == sensor3]
+		data4 = data.loc[data['Address'] == sensor4]
+
+	except Exception:
+		print("TODAY READ CSV ERROR")
+		traceback.print_exc(file=sys.stdout)
+		print('-' * 60)
+
+	try:
+		if temp_plot:
+			fig = plt.figure(plt_size)
+
+			plt.style.use('grayscale')
+
+			plt.plot_date(data0.last(td).index, data0['Temperature'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0', label=sensor0label)
+			plt.plot_date(data1.last(td).index, data1['Temperature'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0.25', label=sensor1label)
+			plt.plot_date(data2.last(td).index, data2['Temperature'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0.5', label=sensor2label)
+			plt.plot_date(data4.last(td).index, data4['Temperature'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0.75', label=sensor4label)
+			plt.legend(loc=0)
+			plt.title('Temperature Plot: Past 16 Hours')
+			plt.xlabel('Time')
+			plt.ylabel('Temp (F)')
+			plt.grid()
+			plt.tight_layout()
+			fig.autofmt_xdate()
+			fig.savefig('/home/pi/Power_Monitoring/output/plot_temp_16.png', bbox_inches='tight')
+		
+		if press_plot:
+			fig = plt.figure(plt_size)
+
+			plt.style.use('grayscale')
+
+			plt.plot_date(data0.last(td).index, data0['Pressure'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0', label=sensor0label)
+			plt.plot_date(data1.last(td).index, data1['Pressure'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0.25', label=sensor1label)
+			plt.plot_date(data2.last(td).index, data2['Pressure'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0.5', label=sensor2label)
+			plt.plot_date(data4.last(td).index, data4['Pressure'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0.75', label=sensor4label)
+			plt.legend(loc=0)
+			plt.title('Pressure Plot: Past 16 Hours')
+			plt.xlabel('Time')
+			plt.ylabel('Pressure (hPa)')
+			plt.grid()
+			plt.tight_layout()
+			fig.autofmt_xdate()
+			fig.savefig('/home/pi/Power_Monitoring/output/plot_press_16.png', bbox_inches='tight')
+			
+		if humid_plot:
+			fig = plt.figure(plt_size)
+
+			plt.style.use('grayscale')
+
+			plt.plot_date(data0.last(td).index, data0['Humidity'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0', label=sensor0label)
+			plt.plot_date(data1.last(td).index, data1['Humidity'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0.25', label=sensor1label)
+			plt.plot_date(data2.last(td).index, data2['Humidity'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0.5', label=sensor2label)
+			plt.plot_date(data4.last(td).index, data4['Humidity'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color='0.75', label=sensor4label)
+			plt.legend(loc=0)
+			plt.title('Humidity Plot: Past 16 Hours')
+			plt.xlabel('Time')
+			plt.ylabel('Humid (%)')
+			plt.grid()
+			plt.tight_layout()
+			fig.autofmt_xdate()
+			fig.savefig('/home/pi/Power_Monitoring/output/plot_humid_16.png', bbox_inches='tight')
+			
+	except Exception:
+		print("MATPLOTLIB ERROR")
+		traceback.print_exc(file=sys.stdout)
+		print('-' * 60) 
