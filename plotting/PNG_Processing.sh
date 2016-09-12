39162d09a41eae1b02a8a4a8dd291c1dd2a957fb
worriedmedic <comical115@gmail.com>
2016-09-12 16:56:02 -0400

    Update PNG_Processing.sh

--- plotting/PNG_Processing.sh
+++ plotting/PNG_Processing.sh
@@ -4,11 +4,19 @@ sleep 20 #Give the python script time to output the images...
 
 cd /home/pi/Power_Monitoring/output
 
-pngcrush -c 0 -ow plot_temp_16.png
-pngcrush -c 0 -ow plot_press_16.png
-pngcrush -c 0 -ow plot_humid_16.png
-sudo cp -f plot_temp_16.png plot_press_16.png plot_humid_16.png /var/www/html/
-sudo chmod 755 /var/www/html/plot_temp_16.png /var/www/html/plot_press_16.png /var/www/html/plot_humid_16.png
+if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
+  pngcrush -reduce -brute -l 9 -ow plot_temp_16.png
+  pngcrush -reduce -brute -l 9 -ow plot_press_16.png
+  pngcrush -reduce -brute -l 9 -ow plot_humid_16.png
+  sudo cp -f plot_temp_16.png plot_press_16.png plot_humid_16.png /var/www/html/
+  sudo chmod 755 /var/www/html/plot_temp_16.png /var/www/html/plot_press_16.png /var/www/html/plot_humid_16.png
+elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
+  pngcrush -c 0 -ow plot_temp_16.png
+  pngcrush -c 0 -ow plot_press_16.png
+  pngcrush -c 0 -ow plot_humid_16.png
+  sudo cp -f plot_temp_16.png plot_press_16.png plot_humid_16.png /var/www/html/
+  sudo chmod 755 /var/www/html/plot_temp_16.png /var/www/html/plot_press_16.png /var/www/html/plot_humid_16.png
+fi
 
 if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
   /usr/local/bin/dropbox_uploader.sh -p upload plot_temp_16.png plot_press_16.png plot_humid_16.png /Programming/logs/dover/plots/
