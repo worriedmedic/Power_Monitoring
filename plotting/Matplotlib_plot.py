323ea5e23407f59e4c9c39ef02f166c3c816b37f
worriedmedic <comical115@gmail.com>
2016-09-12 17:53:47 -0400

    Update Matplotlib_plot.py

--- plotting/Matplotlib_plot.py
+++ plotting/Matplotlib_plot.py
@@ -22,7 +22,7 @@ if os.path.isfile('/home/pi/Power_Monitoring/dover.location'):
 	plt_size_x   = 10
 	plt_size_y   = 8
 	plt_size_dpi = 100
-	plot_style   = 'ggplot' 
+	plot_style   = 'bmh' 
 	sensor0      = '09'
 	sensor0label = 'Outside'
 	sensor1      = '08'
