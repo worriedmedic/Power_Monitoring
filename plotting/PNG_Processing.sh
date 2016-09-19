d94f415af42a610d591523ea55b2dbd5163b337b
worriedmedic <comical115@gmail.com>
2016-09-19 17:00:49 -0400

    Update PNG_Processing.sh

--- plotting/PNG_Processing.sh
+++ plotting/PNG_Processing.sh
@@ -8,18 +8,22 @@ if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
   pngcrush -reduce -brute -l 9 -ow plot_temp.png
   pngcrush -reduce -brute -l 9 -ow plot_press.png
   pngcrush -reduce -brute -l 9 -ow plot_humid.png
-  sudo cp -f plot_temp.png plot_press.png plot_humid.png /var/www/html/
-  sudo chmod 755 /var/www/html/plot_temp.png /var/www/html/plot_press.png /var/www/html/plot_humid.png
+  pngcrush -reduce -brute -l 9 -ow plot_volt.png
+  pngcrush -reduce -brute -l 9 -ow plot_rssi.png
+  sudo cp -f plot_temp.png plot_press.png plot_humid.png plot_volt.png plot_rssi.png /var/www/html/
+  sudo chmod 755 /var/www/html/plot_temp.png /var/www/html/plot_press.png /var/www/html/plot_humid.png /var/www/html/plot_volt.png /var/www/html/plot_rssi.png
 elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
   pngcrush -c 0 -ow plot_temp.png
   pngcrush -c 0 -ow plot_press.png
   pngcrush -c 0 -ow plot_humid.png
-  sudo cp -f plot_temp.png plot_press.png plot_humid.png /var/www/html/
-  sudo chmod 755 /var/www/html/plot_temp.png /var/www/html/plot_press.png /var/www/html/plot_humid.png
+  pngcrush -c 0 -ow plot_volt.png
+  pngcrush -c 0 -ow plot_rssi.png
+  sudo cp -f plot_temp.png plot_press.png plot_humid.png plot_volt.png plot_rssi.png /var/www/html/
+  sudo chmod 755 /var/www/html/plot_temp.png /var/www/html/plot_press.png /var/www/html/plot_humid.png /var/www/html/plot_volt.png /var/www/html/plot_rssi.png
 fi
 
 if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
-  /usr/local/bin/dropbox_uploader.sh -p upload plot_temp.png plot_press.png plot_humid.png /Programming/logs/dover/plots/
+  /usr/local/bin/dropbox_uploader.sh -p upload plot_temp.png plot_press.png plot_humid.png plot_volt.png plot_rssi.png /Programming/logs/dover/plots/
 elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
-  /usr/local/bin/dropbox_uploader.sh -p upload plot_temp.png plot_press.png plot_humid.png /Programming/logs/cuttyhunk/plots/
+  /usr/local/bin/dropbox_uploader.sh -p upload plot_temp.png plot_press.png plot_humid.png plot_volt.png plot_rssi.png /Programming/logs/cuttyhunk/plots/
 fi
