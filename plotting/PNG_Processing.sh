a93ebb7577e8565664ea4430ec0fb43586232273
worriedmedic <comical115@gmail.com>
2016-10-23 08:46:08 -0400

    Update PNG_Processing.sh

--- plotting/PNG_Processing.sh
+++ plotting/PNG_Processing.sh
@@ -5,11 +5,11 @@ sleep 20 #Give the python script time to output the images...
 cd /home/pi/Power_Monitoring/output
 
 if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
-  pngcrush -reduce -brute -l 9 -ow plot_temp.png
-  pngcrush -reduce -brute -l 9 -ow plot_press.png
-  pngcrush -reduce -brute -l 9 -ow plot_humid.png
-  pngcrush -reduce -brute -l 9 -ow plot_volt.png
-  pngcrush -reduce -brute -l 9 -ow plot_rssi.png
+  #pngcrush -reduce -brute -l 9 -ow plot_temp.png
+  #pngcrush -reduce -brute -l 9 -ow plot_press.png
+  #pngcrush -reduce -brute -l 9 -ow plot_humid.png
+  #pngcrush -reduce -brute -l 9 -ow plot_volt.png
+  #pngcrush -reduce -brute -l 9 -ow plot_rssi.png
   sudo cp -f plot_temp.png plot_press.png plot_humid.png plot_volt.png plot_rssi.png /var/www/html/
   sudo chmod 755 /var/www/html/plot_temp.png /var/www/html/plot_press.png /var/www/html/plot_humid.png /var/www/html/plot_volt.png /var/www/html/plot_rssi.png
 elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
