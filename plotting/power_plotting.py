0f9e2295496425562d1b97903b39590ffc5cc56b
worriedmedic <comical115@gmail.com>
2017-01-12 18:14:30 -0500

    Update power_plotting.py

--- plotting/power_plotting.py
+++ plotting/power_plotting.py
@@ -26,3 +26,16 @@ data_4prior = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + prior4.strftim
 data_5prior = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + prior5.strftime("%Y-%m") + '/' + str(today) + 'POWER.log', names = ["Date", "Time", "CT1", "CT2", "CT3", "CT4", "VRMS", "Pulse"])
 data_6prior = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + prior6.strftime("%Y-%m") + '/' + str(today) + 'POWER.log', names = ["Date", "Time", "CT1", "CT2", "CT3", "CT4", "VRMS", "Pulse"])
 data_7prior = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + prior7.strftime("%Y-%m") + '/' + str(today) + 'POWER.log', names = ["Date", "Time", "CT1", "CT2", "CT3", "CT4", "VRMS", "Pulse"])
+
+data = pd.concat([data_7prior, data_6prior, data_5prior, data_4prior, data_3prior, data_2prior, data_yesterday, data_today])
+data['Datetime'] = pd.to_datetime(data['Date'] + ' ' + data['Time'])
+data = data.drop(['Date', 'Time'], 1)
+data = data.set_index('Datetime')
+
+data['CT1'] = data['CT1'].str.replace('ct1:', '')
+data['CT2'] = data['CT2'].str.replace('ct2:', '')
+data['CT3'] = data['CT3'].str.replace('ct3:', '')
+data['CT4'] = data['CT4'].str.replace('ct4:', '')
+data['VRMS'] = data['VRMS'].str.replace('vrms:', '')
+data['Pulse'] = data['Pulse'].str.replace('pulse:', '')
+
