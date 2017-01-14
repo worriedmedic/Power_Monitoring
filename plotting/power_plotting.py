823b193162a7dd871b1112dc97a9519f04181176
worriedmedic <comical115@gmail.com>
2017-01-13 20:23:01 -0500

    Update power_plotting.py

--- plotting/power_plotting.py
+++ plotting/power_plotting.py
@@ -11,8 +11,7 @@ plt_size_x   = 10
 plt_size_y   = 8
 plt_size_dpi = 100
 plot_style   = 'bmh'
-td = '48H'
-line_width = 2
+line_width = 0.5
 rssi_line_width = 1
 label_offset = 3
 
@@ -35,37 +34,67 @@ data_4prior = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + prior4.strftim
 data_5prior = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + prior5.strftime("%Y-%m") + '/' + str(today) + 'POWER.log', names = ["Date", "Time", "CT1", "CT2", "CT3", "CT4", "VRMS", "Pulse"])
 data_6prior = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + prior6.strftime("%Y-%m") + '/' + str(today) + 'POWER.log', names = ["Date", "Time", "CT1", "CT2", "CT3", "CT4", "VRMS", "Pulse"])
 data_7prior = pd.read_csv('/home/pi/Power_Monitoring/data_log/' + prior7.strftime("%Y-%m") + '/' + str(today) + 'POWER.log', names = ["Date", "Time", "CT1", "CT2", "CT3", "CT4", "VRMS", "Pulse"])
-
+  
 data = pd.concat([data_7prior, data_6prior, data_5prior, data_4prior, data_3prior, data_2prior, data_yesterday, data_today])
 data['Datetime'] = pd.to_datetime(data['Date'] + ' ' + data['Time'])
 data = data.drop(['Date', 'Time'], 1)
 data = data.set_index('Datetime')
-
+  
 data['CT1'] = data['CT1'].str.replace('ct1:', '')
 data['CT2'] = data['CT2'].str.replace('ct2:', '')
 data['CT3'] = data['CT3'].str.replace('ct3:', '')
 data['CT4'] = data['CT4'].str.replace('ct4:', '')
 data['VRMS'] = data['VRMS'].str.replace('vrms:', '')
-data['Pulse'] = data['Pulse'].str.replace('pulse:', '')
-
-data = data.astype(float)
-
+data['Pulse'] = data['Pulse'].str.replace('pulse:', '')  
+data = data.astype(float)  
 data['CTT'] = data['CT1'] + data['CT2']
+
+
+td1 = '3H'
+td2 = '24H'
+td3 = '7D'
 
 fig = plt.figure(figsize=(plt_size_x, plt_size_y), dpi=plt_size_dpi)
 plt.style.use(plot_style)
 plt.rcParams['axes.facecolor']='w'
-
-plt.plot_date(data.last(td).index, data['CTT'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color=plt.rcParams['axes.color_cycle'][0], label="CTT")
-plt.plot_date(data.last(td).index, data['CT1'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color=plt.rcParams['axes.color_cycle'][0], label="CT1")
-plt.plot_date(data.last(td).index, data['CT2'].last(td).values, linestyle="solid", linewidth=line_width, marker='None', color=plt.rcParams['axes.color_cycle'][0], label="CT2")
-
+plt.plot_date(data.last(td1).index, data['CTT'].last(td1).values, linestyle="solid", linewidth=line_width, marker='None', color=plt.rcParams['axes.color_cycle'][0], label="CTT")
+plt.fill_between(data.last(td1).index, data['CTT'].last(td1).values, facecolor=plt.rcParams['axes.color_cycle'][0], alpha=0.35)
 plt.legend(loc=2, ncol=2, fontsize=8).set_visible(True)
 plt.title('Dover Power Usage: %s' %td)
 plt.xlabel('Time')
 plt.ylabel('Watts')
-plt.grid(True)
+plt.grid(False)
 plt.tight_layout()
 fig.autofmt_xdate()
 fig.text(0.5, 0.5, 'Dover Power Monitoring', fontsize=25, color='gray', ha='center', va='center', alpha=0.35)
-fig.savefig('/home/pi/Power_Monitoring/output/power.png', bbox_inches='tight')
+fig.savefig('/home/pi/Power_Monitoring/output/%spower.png' %td1, bbox_inches='tight')
+
+fig = plt.figure(figsize=(plt_size_x, plt_size_y), dpi=plt_size_dpi)
+plt.style.use(plot_style)
+plt.rcParams['axes.facecolor']='w'
+plt.plot_date(data.last(td2).index, data['CTT'].last(td2).values, linestyle="solid", linewidth=line_width, marker='None', color=plt.rcParams['axes.color_cycle'][1], label="CTT")
+plt.fill_between(data.last(td2).index, data['CTT'].last(td2).values, facecolor=plt.rcParams['axes.color_cycle'][1], alpha=0.35)
+plt.legend(loc=2, ncol=2, fontsize=8).set_visible(True)
+plt.title('Dover Power Usage: %s' %td)
+plt.xlabel('Time')
+plt.ylabel('Watts')
+plt.grid(False)
+plt.tight_layout()
+fig.autofmt_xdate()
+fig.text(0.5, 0.5, 'Dover Power Monitoring', fontsize=25, color='gray', ha='center', va='center', alpha=0.35)
+fig.savefig('/home/pi/Power_Monitoring/output/%spower.png' %td2, bbox_inches='tight')
+  
+fig = plt.figure(figsize=(plt_size_x, plt_size_y), dpi=plt_size_dpi)
+plt.style.use(plot_style)
+plt.rcParams['axes.facecolor']='w'
+plt.plot_date(data.last(td3).index, data['CTT'].last(td3).values, linestyle="solid", linewidth=line_width, marker='None', color=plt.rcParams['axes.color_cycle'][2], label="CTT")
+plt.fill_between(data.last(td3).index, data['CTT'].last(td3).values, facecolor=plt.rcParams['axes.color_cycle'][2], alpha=0.35)
+plt.legend(loc=2, ncol=2, fontsize=8).set_visible(True)
+plt.title('Dover Power Usage: %s' %td)
+plt.xlabel('Time')
+plt.ylabel('Watts')
+plt.grid(False)
+plt.tight_layout()
+fig.autofmt_xdate()
+fig.text(0.5, 0.5, 'Dover Power Monitoring', fontsize=25, color='gray', ha='center', va='center', alpha=0.35)
+fig.savefig('/home/pi/Power_Monitoring/output/%spower.png' %td3, bbox_inches='tight')
