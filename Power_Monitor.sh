96f7ccecef0a5ef8ca3f72e82b2dfe0babc8668c
worriedmedic <comical115@gmail.com>
2017-04-30 08:30:22 -0400

    Delete Power_Monitor.sh

--- Power_Monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#corruption update
-
-if [ ! -d "/home/pi/Power_Monitoring/data_log" ]; then
-  mkdir /home/pi/Power_Monitoring/data_log
-fi
-
-cd /home/pi/Power_Monitoring
-python Power_Monitor.py >> /home/pi/Power_Monitoring/data_log/Power_Monitor.log 2>&1
