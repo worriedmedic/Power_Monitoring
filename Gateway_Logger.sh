3281dba1b0834adfc6ba6e30d2b1f80a1de3ea9a
worriedmedic <comical115@gmail.com>
2017-04-30 08:30:11 -0400

    Delete Gateway_Logger.sh

--- Gateway_Logger.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ ! -d "/home/pi/Power_Monitoring/data_log" ]; then
-  mkdir /home/pi/Power_Monitoring/data_log
-fi
-cd /home/pi/Power_Monitoring
-python Gateway_Logger.py >> /home/pi/Power_Monitoring/data_log/Gateway_Logger.log 2>&1
