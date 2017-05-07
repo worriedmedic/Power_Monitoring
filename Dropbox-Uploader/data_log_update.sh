a74ae11560d6aa5e2f9d7dec5ec305b1acd13902
worriedmedic <comical115@gmail.com>
2017-05-07 18:17:12 -0400

    Update data_log_update.sh

--- Dropbox-Uploader/data_log_update.sh
+++ Dropbox-Uploader/data_log_update.sh
@@ -10,21 +10,17 @@ if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
   /usr/local/bin/dropbox_uploader.sh -p upload /var/log/network_restart.log /Programming/logs/dover/data_log/sys_log/
   /usr/local/bin/dropbox_uploader.sh -p upload /var/log/fail2ban.log /Programming/logs/dover/data_log/sys_log/
   /usr/local/bin/dropbox_uploader.sh -p upload ~/.ngrok2/_ngrok.log /Programming/logs/dover/data_log/sys_log/
-  /usr/local/bin/dropbox_uploader.sh -p upload ~/.vnc/raspberrypi\:1.log /Programming/logs/dover/data_log/sys_log/
-  
+  /usr/local/bin/dropbox_uploader.sh -p upload ~/.vnc/raspberrypi\:1.log /Programming/logs/dover/data_log/sys_log/ 
   find /home/pi/Power_Monitoring/data_log/* -mtime +8 -exec cp {} /home/pi/Power_Monitoring/data_archive/  \; #Copies any files older than 7 days old to an archive location
   find /home/pi/Power_Monitoring/data_log/* -mtime +8 -delete #Deletes files older than 7 days to prevent extensive dropbox uploads
-  find /home/pi/Power_Monitoring/data_log/* -type d -empty -delete #Deletes empty directories left over
-  
+  find /home/pi/Power_Monitoring/data_log/* -type d -empty -delete #Deletes empty directories left over  
 elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
-  /usr/local/bin/dropbox_uploader.sh -p upload ~/Power_Monitoring/data_log/ /Programming/logs/cuttyhunk/
-  /usr/local/bin/dropbox_uploader.sh -p upload /var/log/network_restart.log /Programming/logs/cuttyhunk/data_log/sys_log/
-  /usr/local/bin/dropbox_uploader.sh -p upload /var/log/fail2ban.log /Programming/logs/cuttyhunk/data_log/sys_log/
-  /usr/local/bin/dropbox_uploader.sh -p upload ~/.ngrok2/ngrok.log /Programming/logs/cuttyhunk/data_log/
-  /usr/local/bin/dropbox_uploader.sh -p upload ~/.vnc/raspberrypi\:1.log /Programming/logs/cuttyhunk/data_log/sys_log/
-  
+  /usr/local/bin/dropbox_uploader.sh -p upload ~/Power_Monitoring/data_log/* /Programming/logs/cuttyhunk/
+  /usr/local/bin/dropbox_uploader.sh -p upload /var/log/network_restart.log /Programming/logs/cuttyhunk/data_log/sys_log/network_restart.log
+  /usr/local/bin/dropbox_uploader.sh -p upload /var/log/fail2ban.log /Programming/logs/cuttyhunk/data_log/sys_log/fail2ban.log
+  /usr/local/bin/dropbox_uploader.sh -p upload ~/.ngrok2/ngrok.log /Programming/logs/cuttyhunk/data_log/ngrok.log
+  /usr/local/bin/dropbox_uploader.sh -p upload ~/.vnc/raspberrypi\:1.log /Programming/logs/cuttyhunk/data_log/sys_log/raspberrypi\:1.log
   find /home/pi/Power_Monitoring/data_log/* -mtime +7 -exec cp {} /home/pi/Power_Monitoring/data_archive/  \; #Copies any files older than 7 days old to an archive location
   find /home/pi/Power_Monitoring/data_log/* -mtime +7 -delete #Deletes files older than 7 days to prevent extensive dropbox uploads
   find /home/pi/Power_Monitoring/data_log/* -type d -empty -delete #Deletes empty directories left over
-  
 fi
