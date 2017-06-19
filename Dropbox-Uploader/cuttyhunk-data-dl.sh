b507c531186e4656726d26652522454e62202697
worriedmedic <comical115@gmail.com>
2017-06-18 22:58:00 -0400

    Update cuttyhunk-data-dl.sh

--- Dropbox-Uploader/cuttyhunk-data-dl.sh
+++ Dropbox-Uploader/cuttyhunk-data-dl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
-  /usr/local/bin/dropbox_uploader.sh -p download /Programming/logs/cuttyhunk/ ~/Power_Monitoring/
+  /usr/local/bin/dropbox_uploader.sh -s -p download /Programming/logs/cuttyhunk/ ~/Power_Monitoring/
   sudo cp /home/pi/Power_Monitoring/cuttyhunk/weather_output.txt /var/www/html/cutty/
   sudo cp /home/pi/Power_Monitoring/cuttyhunk/weather-script-output.png /var/www/html/cutty/
   sudo cp /home/pi/Power_Monitoring/cuttyhunk/weather-script-output.svg /var/www/html/cutty/
