16fc2897bef3d205095b41fc87395705ed4c514f
worriedmedic <comical115@gmail.com>
2017-06-25 09:30:32 -0400

    Update Gateway_Logger.sh

--- Gateway_Logger.sh
+++ Gateway_Logger.sh
@@ -5,4 +5,4 @@ if [ ! -d "/home/pi/Power_Monitoring/data_log" ]; then
 fi
 
 sleep 5
-python /home/pi/Power_Monitoring/Gateway_Logger.py >> /home/pi/Power_Monitoring/data_log/Gateway_Logger.log 2>&1
+python /home/pi/Power_Monitoring/Gateway_Logger.py -vv >> /home/pi/Power_Monitoring/data_log/Gateway_Logger.log 2>&1
