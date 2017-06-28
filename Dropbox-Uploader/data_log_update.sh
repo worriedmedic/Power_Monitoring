0bea5765ee2556ce57f74ca13e44434c4108a29b
worriedmedic <comical115@gmail.com>
2017-06-28 07:59:43 -0400

    Update data_log_update.sh

--- Dropbox-Uploader/data_log_update.sh
+++ Dropbox-Uploader/data_log_update.sh
@@ -7,8 +7,8 @@ fi
 
 if [ -f '/home/pi/Power_Monitoring/dover.location' ]; then
   /usr/local/bin/dropbox_uploader.sh -p upload ~/Power_Monitoring/data_log/ /Programming/logs/dover/
-  find /home/pi/Power_Monitoring/data_log/* -mtime +4 -exec cp {} /home/pi/Power_Monitoring/data_archive/  \; #Copies any files older than 7 days old to an archive location
-  find /home/pi/Power_Monitoring/data_log/* -mtime +4 -delete #Deletes files older than 7 days to prevent extensive dropbox uploads
+  find /home/pi/Power_Monitoring/data_log/* -mtime +8 -exec cp {} /home/pi/Power_Monitoring/data_archive/  \; #Copies any files older than 7 days old to an archive location
+  find /home/pi/Power_Monitoring/data_log/* -mtime +8 -delete #Deletes files older than 7 days to prevent extensive dropbox uploads
   find /home/pi/Power_Monitoring/data_log/* -type d -empty -delete #Deletes empty directories left over
 elif [ -f '/home/pi/Power_Monitoring/cuttyhunk.location' ]; then
   /usr/local/bin/dropbox_uploader.sh -p upload ~/Power_Monitoring/data_log/* /Programming/logs/cuttyhunk/
