cdfb68cbcc3f85928fd94a506c672f4f30761d1e
worriedmedic <comical115@gmail.com>
2017-08-13 20:09:48 -0400

    Update tweet_note.py

--- tweet_note.py
+++ tweet_note.py
@@ -1,4 +1,16 @@
 import time
+from twitter_creds.py import *
+import tweepy
+
+gatewaylog = '/home/pi/Power_Monitoring/data_log/Gateway_Logger.log'
+dataprocesslog = '/home/pi/Power_Monitoring/data_log/Data_to_SVG.log'
+plottinglog = '/home/pi/Power_Monitoring/data_log/Matplotlib-plot.log'
+
+def tweepy_int():
+	api_auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	api = tweepy.API(auth)
+
 
 def follow(thefile):
 	thefile.seek(0,2) # Go to the end of the file
@@ -9,8 +21,15 @@ def follow(thefile):
 			continue
 		yield line
 
+
 def logfollower(filepath):
 	logfile = open(filepath)
 	loglines = follow(logfile)
 	for line in loglines:
 		print line
+		try:
+			if line != '\n':
+      				api.update_status(line)
+		except tweepy.TweepError as e:
+			print(e.reason)
+
