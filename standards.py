17571235aab678c1eacec7dbf9b9a650c835453c
worriedmedic <comical115@gmail.com>
2017-08-14 20:16:13 -0400

    Update standards.py

--- standards.py
+++ standards.py
@@ -1,16 +1,6 @@
 if os.path.isfile('/home/pi/Power_Monitoring/dover.location'):
+	######## GLOBAL VARS ########
 	location = 'dover'
-	today_minus_one = datetime.date.today() + datetime.timedelta(days=-1)
-	today_plus_one = datetime.date.today() + datetime.timedelta(days=1)
-	
-	template_svg_filename = '/home/pi/Power_Monitoring/resources/DOVER_WX_TEMPLATE.svg'
-	
-	tide = False
-	
-	wunder_site_forecast_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/forecast/q/ny/carmel.json'
-	wunder_site_astronomy_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/astronomy/q/ny/carmel.json'
-	wunder_site_conditions_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/conditions/q/ny/carmel.json'
-	
 	sensor0		= '09'
 	sensor0label	= 'Outside'
 	sensor1		= '08'
@@ -27,22 +17,23 @@ if os.path.isfile('/home/pi/Power_Monitoring/dover.location'):
 	sensor6label	= 'Laundry'
 	sensor7		= '02'
 	sensor7label	= 'Liam'
-	if verbose:
-		print(location)
-	
+	######## GATEWAY VARS ########
+	gateway_addr = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A104VAFG-if00-port0'
+	######## DATA VARS ########
+	template_svg_filename = '/home/pi/Power_Monitoring/resources/DOVER_WX_TEMPLATE.svg'
+	tide = False
+	wunder_site_forecast_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/forecast/q/ny/carmel.json'
+	wunder_site_astronomy_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/astronomy/q/ny/carmel.json'
+	wunder_site_conditions_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/conditions/q/ny/carmel.json'
+	######## PLOTTING VARS ########
+	td           = '48H'
+	plt_size_x   = 10
+	plt_size_y   = 8
+	plt_size_dpi = 100
+	plot_style   = 'bmh'
 elif os.path.isfile('/home/pi/Power_Monitoring/cuttyhunk.location'):
+	######## GLOBAL VARS ########
 	location = 'cuttyhunk'
-	today_minus_one = datetime.date.today() + datetime.timedelta(days=-1)
-	today_plus_one = datetime.date.today() + datetime.timedelta(days=1)
-	
-	tide = True
-	
-	template_svg_filename = '/home/pi/Power_Monitoring/resources/CUTTY_WX_TEMPLATE.svg'
-	
-	wunder_site_forecast_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/forecast/q/ma/cuttyhunk.json'
-	wunder_site_astronomy_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/astronomy/q/ma/cuttyhunk.json'
-	wunder_site_conditions_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/conditions/q/ma/cuttyhunk.json'
-	
 	sensor0		= '98'
 	sensor0label	= 'Outside'
 	sensor1		= '96'
@@ -59,5 +50,17 @@ elif os.path.isfile('/home/pi/Power_Monitoring/cuttyhunk.location'):
 	sensor6label	= 'None'
 	sensor7		= '86'
 	sensor7label	= 'None'
-	if verbose:
-		print(location)
+	######## GATEWAY VARS ########
+	gateway_addr = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AL02CC7C-if00-port0'
+	######## DATA VARS ########
+	tide = True
+	template_svg_filename = '/home/pi/Power_Monitoring/resources/CUTTY_WX_TEMPLATE.svg'
+	wunder_site_forecast_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/forecast/q/ma/cuttyhunk.json'
+	wunder_site_astronomy_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/astronomy/q/ma/cuttyhunk.json'
+	wunder_site_conditions_json = 'http://api.wunderground.com/api/1f86b1c989ac268c/conditions/q/ma/cuttyhunk.json'
+	######## PLOTTING VARS ########
+	td           = '72H'
+	plt_size_x   = 10
+	plt_size_y   = 8
+	plt_size_dpi = 100
+	plot_style   = 'bmh'
