0b2445c936c982265b332fb189b2eb51ead1a8cb
worriedmedic <comical115@gmail.com>
2017-08-16 23:07:32 -0400

    Update display.py

--- display.py
+++ display.py
@@ -56,209 +56,218 @@ class SmDisplay:
 		'''DESTRUCTOR'''
 	
 	def pickle_update(self):
-		location = '/home/pi/Power_Monitoring/pickle/'
-		self.data = pickle.load(open('total_pickle.p', "rb"))
+		try:
+			location = '/home/pi/Power_Monitoring/pickle/'
+			self.data = pickle.load(open('total_pickle.p', "rb"))
+		except Exception:
+			print("ERROR: Pickle Update")
 	
 	def wx_disp(self, data):
-		self.screen.fill(black)
-		xmin = 10
-		xmax = self.xmax
-		ymax = self.ymax
-		fn = "freesans"
-		lines = 2
-		lc = (255,255,255)
-		
-		################################################################################
-		pygame.draw.line( self.screen, lc, (xmin,0),(xmax,0), lines ) #Top Line
-		pygame.draw.line( self.screen, lc, (xmin,0),(xmin,ymax), lines ) #Left Line
-		pygame.draw.line( self.screen, lc, (xmin,ymax),(xmax,ymax), lines ) #Bottom Line
-		pygame.draw.line( self.screen, lc, (xmax,0),(xmax,ymax), lines ) #Right Line
-		pygame.draw.line( self.screen, lc, (xmin,ymax*0.15),(xmax,ymax*0.15), lines ) #Horizontal Line Under Top
-		
-		font = pygame.font.SysFont( fn, int(ymax*(self.txth)), bold=1 )
-		lfont = pygame.font.SysFont( fn, int(135), bold=1 )
-		sfont = pygame.font.SysFont( fn, int(ymax*(self.smtxth)), bold=1 )
-		smfont = pygame.font.SysFont(fn, int(ymax*(self.smrtxth)), bold=1)
-		tinyfont = pygame.font.SysFont(fn, int(16), bold=1)
-		
-		tm1 = time.strftime("%a, %b %d %H:%M:%S", time.localtime() )
-		
-		rtm1 = font.render(tm1, True, lc)
-		
-		self.screen.blit(rtm1, (15,self.dateYPos))
-		
-		weather_data = {'sunrise'	: self.data[8]['sunrise'],
-				'sunset'	: self.data[8]['sunset']}
-		
-		sunrise = smfont.render(weather_data['sunrise'], True, lc)
-		(srx1, sry1) = sunrise.get_size()
-		sunset = smfont.render(weather_data['sunset'], True, lc)
-		(ssx1, ssy1) = sunset.get_size()
-		
-		self.screen.blit(sunrise, (428,2))
-		self.screen.blit(sunset, (417,20))
-		################################################################################
-		pygame.draw.line(self.screen, lc, (310,ymax*0.15),(310,ymax*0.6), lines) #Vertical Line @x:310
-		pygame.draw.line(self.screen, lc, (xmin,ymax*0.6),(xmax,ymax*0.6), lines)
-		
-		if self.data[data]['temperature'][0] < 100:
-			temp = str(self.data[data]['temperature'][0])[:4]
-		elif self.data[data]['temperature'][0] >= 100:
-			temp = str(self.data[data]['temperature'][0])[:3]
-		rtemp = lfont.render(temp, True, lc)
-		self.screen.blit(rtemp, (18, 32))
-		
-		rsensorlabel = font.render(self.data[data]['name'][:7], True, lc)
-		self.screen.blit(rsensorlabel, (317,50))
-		
-		dfont = pygame.font.SysFont(fn, int(40), bold=1 )
-		dtxt = dfont.render(uniTmp, True, lc )
-		self.screen.blit(dtxt, (270, 50))
-		
-		rtemptime = tinyfont.render(self.data[data]['time'].strftime("%y-%m-%d %H:%M:%S"), True, lc)
-		self.screen.blit(rtemptime, (15, 165))
-		
-		rtempmax = tinyfont.render('Max: ' + str(self.data[data]['temperature_max'])[:4], True, lc)
-		self.screen.blit(rtempmax, (162, 165))
-		
-		rtempmin = tinyfont.render('Min: ' + str(self.data[data]['temperature_min'])[:4], True, lc)
-		self.screen.blit(rtempmin, (238, 165))
-		################################################################################
-		tempgraph = pygame.image.load(os.path.join('/home/pi/Power_Monitoring/output/', 'plot_sm_Temperature_sensor_%s.png' %data))
-		tempgraph2 = pygame.transform.scale(tempgraph, (455, 122))
-		self.screen.blit(tempgraph2, (12,192))
-		################################################################################
-		if self.data[data]['humidity'][0] == 100:
-			humid = str(self.data[data]['humidity'][0])[:3]
-		elif self.data[data]['humidity'][0] < 100:
-			humid = str(self.data[data]['humidity'][0])[:4]
-		rhumid = tinyfont.render(humid + '%', True, lc)
-		self.screen.blit(rhumid, (400, 50+50))
-		rhumid_label = tinyfont.render('Humid:', True, lc)
-		self.screen.blit(rhumid_label, (317, 50+50))
-		
-		if self.data[data]['dewpoint'][0] == 100:
-			dew = str(self.data[data]['dewpoint'][0])[:3]
-		elif self.data[data]['dewpoint'][0] < 100:
-			dew = str(self.data[data]['dewpoint'][0])[:4]
-		rdew = tinyfont.render(dew + " " + uniTmp, True, lc)
-		self.screen.blit(rdew, (400,67+50))
-		rdew_label = tinyfont.render('Dewpoint:', True, lc)
-		self.screen.blit(rdew_label, (317,67+50))
-		
-		if self.data[data]['pressure'][0] >= 1000:
-			press = str(self.data[data]['pressure'][0])[:4]
-		elif self.data[data]['dewpoint'][0] < 1000:
-			press = str(self.data[data]['pressure'][0])[:5]
-		rpress = tinyfont.render(press + "Hg", True, lc)
-		self.screen.blit(rpress, (400,84+50))
-		rpress_label = tinyfont.render('Pressure:', True, lc)
-		self.screen.blit(rpress_label, (317,84+50))
-		
-		volt = str(self.data[data]['voltage'][0])[:4]
-		rvolt = tinyfont.render(volt + "vdc", True, lc)
-		self.screen.blit(rvolt, (400,101+50))
-		rvolt_label = tinyfont.render('Voltage:', True, lc)
-		self.screen.blit(rvolt_label, (317,101+50))
-		
-		rssi = str(self.data[data]['rssi'][0])[:2]
-		rrssi = tinyfont.render(rssi, True, lc)
-		self.screen.blit(rrssi, (400,118+50))
-		rrssi_label = tinyfont.render('RSSI:', True, lc)
-		self.screen.blit(rrssi_label, (317,118+50))
-		
-		################################################################################
-		pygame.display.update()
-		################################################################################
+		try:
+			self.screen.fill(black)
+			xmin = 10
+			xmax = self.xmax
+			ymax = self.ymax
+			fn = "freesans"
+			lines = 2
+			lc = (255,255,255)
+			
+			################################################################################
+			pygame.draw.line( self.screen, lc, (xmin,0),(xmax,0), lines ) #Top Line
+			pygame.draw.line( self.screen, lc, (xmin,0),(xmin,ymax), lines ) #Left Line
+			pygame.draw.line( self.screen, lc, (xmin,ymax),(xmax,ymax), lines ) #Bottom Line
+			pygame.draw.line( self.screen, lc, (xmax,0),(xmax,ymax), lines ) #Right Line
+			pygame.draw.line( self.screen, lc, (xmin,ymax*0.15),(xmax,ymax*0.15), lines ) #Horizontal Line Under Top
+			
+			font = pygame.font.SysFont( fn, int(ymax*(self.txth)), bold=1 )
+			lfont = pygame.font.SysFont( fn, int(135), bold=1 )
+			sfont = pygame.font.SysFont( fn, int(ymax*(self.smtxth)), bold=1 )
+			smfont = pygame.font.SysFont(fn, int(ymax*(self.smrtxth)), bold=1)
+			tinyfont = pygame.font.SysFont(fn, int(16), bold=1)
+			
+			tm1 = time.strftime("%a, %b %d %H:%M:%S", time.localtime() )
+			
+			rtm1 = font.render(tm1, True, lc)
+			
+			self.screen.blit(rtm1, (15,self.dateYPos))
+			
+			weather_data = {'sunrise'	: self.data[8]['sunrise'],
+					'sunset'	: self.data[8]['sunset']}
+			
+			sunrise = smfont.render(weather_data['sunrise'], True, lc)
+			(srx1, sry1) = sunrise.get_size()
+			sunset = smfont.render(weather_data['sunset'], True, lc)
+			(ssx1, ssy1) = sunset.get_size()
+			
+			self.screen.blit(sunrise, (428,2))
+			self.screen.blit(sunset, (417,20))
+			################################################################################
+			pygame.draw.line(self.screen, lc, (310,ymax*0.15),(310,ymax*0.6), lines) #Vertical Line @x:310
+			pygame.draw.line(self.screen, lc, (xmin,ymax*0.6),(xmax,ymax*0.6), lines)
+			
+			if self.data[data]['temperature'][0] < 100:
+				temp = str(self.data[data]['temperature'][0])[:4]
+			elif self.data[data]['temperature'][0] >= 100:
+				temp = str(self.data[data]['temperature'][0])[:3]
+			rtemp = lfont.render(temp, True, lc)
+			self.screen.blit(rtemp, (18, 32))
+			
+			rsensorlabel = font.render(self.data[data]['name'][:7], True, lc)
+			self.screen.blit(rsensorlabel, (317,50))
+			
+			dfont = pygame.font.SysFont(fn, int(40), bold=1 )
+			dtxt = dfont.render(uniTmp, True, lc )
+			self.screen.blit(dtxt, (270, 50))
+			
+			rtemptime = tinyfont.render(self.data[data]['time'].strftime("%y-%m-%d %H:%M:%S"), True, lc)
+			self.screen.blit(rtemptime, (15, 165))
+			
+			rtempmax = tinyfont.render('Max: ' + str(self.data[data]['temperature_max'])[:4], True, lc)
+			self.screen.blit(rtempmax, (162, 165))
+			
+			rtempmin = tinyfont.render('Min: ' + str(self.data[data]['temperature_min'])[:4], True, lc)
+			self.screen.blit(rtempmin, (238, 165))
+			################################################################################
+			tempgraph = pygame.image.load(os.path.join('/home/pi/Power_Monitoring/output/', 'plot_sm_Temperature_sensor_%s.png' %data))
+			tempgraph2 = pygame.transform.scale(tempgraph, (455, 122))
+			self.screen.blit(tempgraph2, (12,192))
+			################################################################################
+			if self.data[data]['humidity'][0] == 100:
+				humid = str(self.data[data]['humidity'][0])[:3]
+			elif self.data[data]['humidity'][0] < 100:
+				humid = str(self.data[data]['humidity'][0])[:4]
+			rhumid = tinyfont.render(humid + '%', True, lc)
+			self.screen.blit(rhumid, (400, 50+50))
+			rhumid_label = tinyfont.render('Humid:', True, lc)
+			self.screen.blit(rhumid_label, (317, 50+50))
+			
+			if self.data[data]['dewpoint'][0] == 100:
+				dew = str(self.data[data]['dewpoint'][0])[:3]
+			elif self.data[data]['dewpoint'][0] < 100:
+				dew = str(self.data[data]['dewpoint'][0])[:4]
+			rdew = tinyfont.render(dew + " " + uniTmp, True, lc)
+			self.screen.blit(rdew, (400,67+50))
+			rdew_label = tinyfont.render('Dewpoint:', True, lc)
+			self.screen.blit(rdew_label, (317,67+50))
+			
+			if self.data[data]['pressure'][0] >= 1000:
+				press = str(self.data[data]['pressure'][0])[:4]
+			elif self.data[data]['dewpoint'][0] < 1000:
+				press = str(self.data[data]['pressure'][0])[:5]
+			rpress = tinyfont.render(press + "Hg", True, lc)
+			self.screen.blit(rpress, (400,84+50))
+			rpress_label = tinyfont.render('Pressure:', True, lc)
+			self.screen.blit(rpress_label, (317,84+50))
+			
+			volt = str(self.data[data]['voltage'][0])[:4]
+			rvolt = tinyfont.render(volt + "vdc", True, lc)
+			self.screen.blit(rvolt, (400,101+50))
+			rvolt_label = tinyfont.render('Voltage:', True, lc)
+			self.screen.blit(rvolt_label, (317,101+50))
+			
+			rssi = str(self.data[data]['rssi'][0])[:2]
+			rrssi = tinyfont.render(rssi, True, lc)
+			self.screen.blit(rrssi, (400,118+50))
+			rrssi_label = tinyfont.render('RSSI:', True, lc)
+			self.screen.blit(rrssi_label, (317,118+50))
+			
+			################################################################################
+			pygame.display.update()
+			################################################################################
+		except Exception:
+			print('ERROR: WX DISPLAY')
 	
 	def tide_disp(self):
-		self.screen.fill(black)
-		xmin = 10
-		xmax = self.xmax
-		ymax = self.ymax
-		fn = "freesans"
-		lines = 2
-		lc = (255,255,255)
-		
-		################################################################################
-		pygame.draw.line( self.screen, lc, (xmin,0),(xmax,0), lines ) #Top Line
-		pygame.draw.line( self.screen, lc, (xmin,0),(xmin,ymax), lines ) #Left Line
-		pygame.draw.line( self.screen, lc, (xmin,ymax),(xmax,ymax), lines ) #Bottom Line
-		pygame.draw.line( self.screen, lc, (xmax,0),(xmax,ymax), lines ) #Right Line
-		pygame.draw.line( self.screen, lc, (xmin,ymax*0.15),(xmax,ymax*0.15), lines ) #Horizontal Line Under Top
-		
-		font = pygame.font.SysFont(fn, int(ymax*(self.txth)), bold=1 )
-		tidefont = pygame.font.SysFont(fn, int(75), bold=1 )
-		lfont = pygame.font.SysFont(fn, int(135), bold=1 )
-		sfont = pygame.font.SysFont(fn, int(40), bold=1 )
-		smfont = pygame.font.SysFont(fn, int(ymax*(self.smrtxth)), bold=1)
-		tinyfont = pygame.font.SysFont(fn, int(16), bold=1)
-		
-		tm1 = time.strftime("%a, %b %d %H:%M:%S", time.localtime() )
-		
-		rtm1 = font.render(tm1, True, lc)
-		
-		self.screen.blit(rtm1, (15,self.dateYPos))
-		
-		weather_data = {'sunrise'	: self.data[8]['sunrise'],
-				'sunset'	: self.data[8]['sunset']}
-		
-		sunrise = smfont.render(weather_data['sunrise'], True, lc)
-		(srx1, sry1) = sunrise.get_size()
-		sunset = smfont.render(weather_data['sunset'], True, lc)
-		(ssx1, ssy1) = sunset.get_size()
-		
-		self.screen.blit(sunrise, (428,2))
-		self.screen.blit(sunset, (417,20))
-		################################################################################
-		tide_title = tidefont.render('Tides', True, lc)
-		self.screen.blit(tide_title, (20, 50))
-		
-		icon_tide = pygame.image.load(os.path.join('/home/pi/Power_Monitoring/resources/', 'tide.png'))
-		icon_tide_2 = pygame.transform.scale(icon_tide, (155, 85))
-		self.screen.blit(icon_tide_2, (275, 60))
-		
-		td_pr_yh = 150
-		rtd_pr = tinyfont.render('Tide Prior', True, lc)
-		self.screen.blit(rtd_pr, (20, td_pr_yh - 15))
-		rtd_pr_tm = sfont.render(str(self.data[9]['tide_prior_time'].strftime("%H:%M")), True, lc)
-		self.screen.blit(rtd_pr_tm, (20, td_pr_yh))
-		rtd_pr_ty = sfont.render(self.data[9]['tide_prior_type'], True, lc)
-		self.screen.blit(rtd_pr_ty, (140, td_pr_yh))
-		rtd_pr_lv = sfont.render(str(self.data[9]['tide_prior_level'])[:4], True, lc)
-		self.screen.blit(rtd_pr_lv, (220, td_pr_yh))
-		td_pr_cn = datetime.datetime.now() - self.data[9]['tide_prior_time']
-		rtd_pr_cn = sfont.render(str(td_pr_cn)[:7], True, lc)
-		self.screen.blit(rtd_pr_cn, (320, td_pr_yh))
-		
-		td_nx_yh = 150 + 60
-		rtd_nx = tinyfont.render('Tide Next', True, lc)
-		self.screen.blit(rtd_nx, (20, td_nx_yh - 15))
-		rtd_nx_tm = sfont.render(str(self.data[9]['tide_next_time'].strftime("%H:%M")), True, lc)
-		self.screen.blit(rtd_nx_tm, (20, td_nx_yh))
-		rtd_nx_ty = sfont.render(self.data[9]['tide_next_type'], True, lc)
-		self.screen.blit(rtd_nx_ty, (140, td_nx_yh))
-		rtd_nx_lv = sfont.render(str(self.data[9]['tide_next_level'])[:4], True, lc)
-		self.screen.blit(rtd_nx_lv, (220, td_nx_yh))
-		td_nx_cn = self.data[9]['tide_next_time'] - datetime.datetime.now()
-		rtd_nx_cn = sfont.render(str(td_nx_cn)[:7], True, lc)
-		self.screen.blit(rtd_nx_cn, (320, td_nx_yh))
-		
-		td_af_yh = 150 + 120
-		rtd_af = tinyfont.render('Tide Next', True, lc)
-		self.screen.blit(rtd_af, (20, td_af_yh - 15))
-		rtd_af_tm = sfont.render(str(self.data[9]['tide_after_time'].strftime("%H:%M")), True, lc)
-		self.screen.blit(rtd_af_tm, (20, td_af_yh))
-		rtd_af_ty = sfont.render(self.data[9]['tide_after_type'], True, lc)
-		self.screen.blit(rtd_af_ty, (140, td_af_yh))
-		rtd_af_lv = sfont.render(str(self.data[9]['tide_after_level'])[:4], True, lc)
-		self.screen.blit(rtd_af_lv, (220, td_af_yh))
-		td_af_cn = self.data[9]['tide_after_time'] - datetime.datetime.now()
-		rtd_af_cn = sfont.render(str(td_af_cn)[:7], True, lc)
-		self.screen.blit(rtd_af_cn, (320, td_af_yh))
-		################################################################################
-		pygame.display.update()
-		################################################################################
+		try:
+			self.screen.fill(black)
+			xmin = 10
+			xmax = self.xmax
+			ymax = self.ymax
+			fn = "freesans"
+			lines = 2
+			lc = (255,255,255)
+			
+			################################################################################
+			pygame.draw.line( self.screen, lc, (xmin,0),(xmax,0), lines ) #Top Line
+			pygame.draw.line( self.screen, lc, (xmin,0),(xmin,ymax), lines ) #Left Line
+			pygame.draw.line( self.screen, lc, (xmin,ymax),(xmax,ymax), lines ) #Bottom Line
+			pygame.draw.line( self.screen, lc, (xmax,0),(xmax,ymax), lines ) #Right Line
+			pygame.draw.line( self.screen, lc, (xmin,ymax*0.15),(xmax,ymax*0.15), lines ) #Horizontal Line Under Top
+			
+			font = pygame.font.SysFont(fn, int(ymax*(self.txth)), bold=1 )
+			tidefont = pygame.font.SysFont(fn, int(75), bold=1 )
+			lfont = pygame.font.SysFont(fn, int(135), bold=1 )
+			sfont = pygame.font.SysFont(fn, int(40), bold=1 )
+			smfont = pygame.font.SysFont(fn, int(ymax*(self.smrtxth)), bold=1)
+			tinyfont = pygame.font.SysFont(fn, int(16), bold=1)
+			
+			tm1 = time.strftime("%a, %b %d %H:%M:%S", time.localtime() )
+			
+			rtm1 = font.render(tm1, True, lc)
+			
+			self.screen.blit(rtm1, (15,self.dateYPos))
+			
+			weather_data = {'sunrise'	: self.data[8]['sunrise'],
+					'sunset'	: self.data[8]['sunset']}
+			
+			sunrise = smfont.render(weather_data['sunrise'], True, lc)
+			(srx1, sry1) = sunrise.get_size()
+			sunset = smfont.render(weather_data['sunset'], True, lc)
+			(ssx1, ssy1) = sunset.get_size()
+			
+			self.screen.blit(sunrise, (428,2))
+			self.screen.blit(sunset, (417,20))
+			################################################################################
+			tide_title = tidefont.render('Tides', True, lc)
+			self.screen.blit(tide_title, (20, 50))
+			
+			icon_tide = pygame.image.load(os.path.join('/home/pi/Power_Monitoring/resources/', 'tide.png'))
+			icon_tide_2 = pygame.transform.scale(icon_tide, (155, 85))
+			self.screen.blit(icon_tide_2, (275, 60))
+			
+			td_pr_yh = 150
+			rtd_pr = tinyfont.render('Tide Prior', True, lc)
+			self.screen.blit(rtd_pr, (20, td_pr_yh - 15))
+			rtd_pr_tm = sfont.render(str(self.data[9]['tide_prior_time'].strftime("%H:%M")), True, lc)
+			self.screen.blit(rtd_pr_tm, (20, td_pr_yh))
+			rtd_pr_ty = sfont.render(self.data[9]['tide_prior_type'], True, lc)
+			self.screen.blit(rtd_pr_ty, (140, td_pr_yh))
+			rtd_pr_lv = sfont.render(str(self.data[9]['tide_prior_level'])[:4], True, lc)
+			self.screen.blit(rtd_pr_lv, (220, td_pr_yh))
+			td_pr_cn = datetime.datetime.now() - self.data[9]['tide_prior_time']
+			rtd_pr_cn = sfont.render(str(td_pr_cn)[:7], True, lc)
+			self.screen.blit(rtd_pr_cn, (320, td_pr_yh))
+			
+			td_nx_yh = 150 + 60
+			rtd_nx = tinyfont.render('Tide Next', True, lc)
+			self.screen.blit(rtd_nx, (20, td_nx_yh - 15))
+			rtd_nx_tm = sfont.render(str(self.data[9]['tide_next_time'].strftime("%H:%M")), True, lc)
+			self.screen.blit(rtd_nx_tm, (20, td_nx_yh))
+			rtd_nx_ty = sfont.render(self.data[9]['tide_next_type'], True, lc)
+			self.screen.blit(rtd_nx_ty, (140, td_nx_yh))
+			rtd_nx_lv = sfont.render(str(self.data[9]['tide_next_level'])[:4], True, lc)
+			self.screen.blit(rtd_nx_lv, (220, td_nx_yh))
+			td_nx_cn = self.data[9]['tide_next_time'] - datetime.datetime.now()
+			rtd_nx_cn = sfont.render(str(td_nx_cn)[:7], True, lc)
+			self.screen.blit(rtd_nx_cn, (320, td_nx_yh))
+			
+			td_af_yh = 150 + 120
+			rtd_af = tinyfont.render('Tide Next', True, lc)
+			self.screen.blit(rtd_af, (20, td_af_yh - 15))
+			rtd_af_tm = sfont.render(str(self.data[9]['tide_after_time'].strftime("%H:%M")), True, lc)
+			self.screen.blit(rtd_af_tm, (20, td_af_yh))
+			rtd_af_ty = sfont.render(self.data[9]['tide_after_type'], True, lc)
+			self.screen.blit(rtd_af_ty, (140, td_af_yh))
+			rtd_af_lv = sfont.render(str(self.data[9]['tide_after_level'])[:4], True, lc)
+			self.screen.blit(rtd_af_lv, (220, td_af_yh))
+			td_af_cn = self.data[9]['tide_after_time'] - datetime.datetime.now()
+			rtd_af_cn = sfont.render(str(td_af_cn)[:7], True, lc)
+			self.screen.blit(rtd_af_cn, (320, td_af_yh))
+			################################################################################
+			pygame.display.update()
+			################################################################################
+		except Exception:
+			print("ERROR: TIDE DISPLAY")
 
 if (1):
 	d = SmDisplay()
