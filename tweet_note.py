a6192ea3622e0e17a878bb3a616632a889038b0f
worriedmedic <comical115@gmail.com>
2017-08-18 15:22:53 -0400

    Update tweet_note.py

--- tweet_note.py
+++ tweet_note.py
@@ -6,6 +6,7 @@ import threading
 gatewaylog = '/home/pi/Power_Monitoring/data_log/Gateway_Logger.log'
 dataprocesslog = '/home/pi/Power_Monitoring/data_log/Data_to_SVG.log'
 plottinglog = '/home/pi/Power_Monitoring/data_log/Matplotlib-plot.log'
+displaylog = '/home/pi/Power_Monitoring/data_log/display.log'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -22,20 +23,31 @@ def follow(thefile):
 
 
 def logfollower(filepath,loc):
-	logfile = open(filepath)
-	loglines = follow(logfile)
-	for line in loglines:
-		print line
-		try:
-			if line.startswith("('ERROR") or line.startswith('serial'):
-				api.update_status(loc + line[:130])
-		except tweepy.TweepError as e:
-			print(e.reason)
+	try:
+		logfile = open(filepath)
+		loglines = follow(logfile)
+		for line in loglines:
+			print line
+			try:
+				if line.startswith("('ERROR") or line.startswith('serial'):
+					api.update_status(loc + line[:130])
+			except tweepy.TweepError as e:
+				print(e.reason)
+	except Exception:
+		print("ERROR: LOGFOLLOWER DEF", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+		traceback.print_exc(file=sys.stdout)
 
 tgateway = threading.Thread(target=logfollower, args=(gatewaylog,'GATE:',))
 tdataproc = threading.Thread(target=logfollower, args=(dataprocesslog,'DATA:',))
 tplotting = threading.Thread(target=logfollower, args=(plottinglog,'PLOT:'))
+tdisplay = threading.Thread(target=logfollower, args=(displaylog,'DISP:'))
+
+tgateway.daemon = True
+tdataproc.daemon = True
+tplotting.daemon = True
+tdisplay.daemon = True
 
 tgateway.start()
 tdataproc.start()
 tplotting.start()
+tdisplay.start()
