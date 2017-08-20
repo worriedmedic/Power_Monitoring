5eff3af2d190ef08416e13d790d3361c4846fbc7
worriedmedic <comical115@gmail.com>
2017-08-20 08:33:14 -0400

    Update display.sh

--- display.sh
+++ display.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd /home/pi/Power_Monitoring
+sleep 10
 export DISPLAY=:0
-python display.py >> ./data_log/display.log 2>&1
+python display.py >> /home/pi/Power_Monitoring/data_log/display.log 2>&1
