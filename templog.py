24effee1f42f39ebf5f62d4d61efdc4bce72f437
worriedmedic <comical115@gmail.com>
2017-08-27 16:00:07 -0400

    Update templog.py

--- templog.py
+++ templog.py
@@ -7,7 +7,7 @@ def datawrite(temp):
 	with open("/home/pi/Power_Monitoring/data_log/cpu_temp.csv", "a") as log:
 		log.write("{0},{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
 
-def graph():
+def graph(temp):
 	y.append(temp)
 	x.append(time())
 	plt.clf()
