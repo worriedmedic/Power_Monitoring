dd9240524e7ce58943301459eaf7e9152d89f082
worriedmedic <comical115@gmail.com>
2017-08-27 15:59:27 -0400

    Update templog.py

--- templog.py
+++ templog.py
@@ -1,19 +1,7 @@
 #/bin/python
-import datetime, time,
+import datetime, time
 from gpiozero import CPUTemperature
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import subprocess
-from apscheduler.schedulers.blocking import BlockingScheduler
-import logging
-logging.basicConfig()
-from standards import *
-
-
-line_width = 1.5
-rssi_line_width = 1
-label_offset = 3
-running = True
 
 def datawrite(temp):
 	with open("/home/pi/Power_Monitoring/data_log/cpu_temp.csv", "a") as log:
@@ -27,6 +15,7 @@ def graph():
 	plt.plot(x,y)
 	plt.draw() 
 
+running = True
 while running:
 	temp = CPUTemperature()
 	datawrite(temp)
