585597d2e2cdd7b45c984a4fdcae1dda92b413d2
worriedmedic <comical115@gmail.com>
2017-08-27 16:01:51 -0400

    Update templog.py

--- templog.py
+++ templog.py
@@ -3,6 +3,10 @@ import datetime, time
 from gpiozero import CPUTemperature
 import matplotlib.pyplot as plt
 
+plt.ion()
+x = []
+y = []
+
 def datawrite(temp):
 	with open("/home/pi/Power_Monitoring/data_log/cpu_temp.csv", "a") as log:
 		log.write("{0},{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
@@ -19,5 +23,5 @@ running = True
 while running:
 	temp = CPUTemperature()
 	datawrite(temp)
-	graph(temp)
+	#graph(temp)
 	sleep(10)
