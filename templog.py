687bba770f087cdd34eee8e4f4ceb990dc3a680f
worriedmedic <comical115@gmail.com>
2017-08-27 21:03:41 -0400

    Update templog.py

--- templog.py
+++ templog.py
@@ -1,27 +1,13 @@
 #/bin/python
 import datetime, time
 from gpiozero import CPUTemperature
-import matplotlib.pyplot as plt
-
-plt.ion()
-x = []
-y = []
 
 def datawrite(temp):
 	with open("/home/pi/Power_Monitoring/data_log/cpu_temp.csv", "a") as log:
-		log.write("{0},{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
-
-def graph(temp):
-	y.append(temp)
-	x.append(time.time ())
-	plt.clf()
-	plt.scatter(x,y)
-	plt.plot(x,y)
-	plt.draw() 
+		log.write("{0},{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(temp).split('=',1)[1][:5]))
 
 running = True
 while running:
 	temp = CPUTemperature()
 	datawrite(temp)
-	#graph(temp)
 	time.sleep(10)
